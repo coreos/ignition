a8fdc487f64b542c5a2f0a68bca90ae3c40722b0
Greg Kroah-Hartman <greg@kroah.com>
2013-07-17 17:41:24 -0700

    feat(dracut) add proper support for cgpt
    This now looks at the partition, lowercases the name, mounts the
    partition, and then kexecs the "real" kernel that should be run.

--- dracut/80gptprio/parse-gptprio.sh
+++ dracut/80gptprio/parse-gptprio.sh
@@ -4,6 +4,8 @@
 
 if [ "${root%%:*}" = "gptprio" ]; then
     rootok=1
-    # TODO: wait on the coreos-root by-uuid type instead
+
+    # Wait for both root partitions to show up before we move on
     wait_for_dev "/dev/disk/by-partuuid/7130c94a-213a-4e5a-8e26-6cce9662f132"
+    wait_for_dev "/dev/disk/by-partuuid/e03dd35c-7c2d-4a47-b3fe-27f15780a57c"
 fi
