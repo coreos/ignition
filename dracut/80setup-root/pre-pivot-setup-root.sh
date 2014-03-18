c39d1e5bb6e11e9a9822f053541015b09e86f5ff
Michael Marineau <michael.marineau@coreos.com>
2014-03-18 14:49:19 -0700

    add(80setup-root): Move fs setup logic to new module
    This module only runs on /usr images. The old manual symlink creation
    logic has been replaced by calling systemd-tmpfiles with it's new shiny
    --root argument. Only rules from baselayout files are evaluated to avoid
    polluting the root file system just in case some places like /home have
    been configured to be mount points.

---
+++ dracut/80setup-root/pre-pivot-setup-root.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+# /etc/machine-id after a new image is created:
+COREOS_BLANK_MACHINE_ID="42000000000000000000000000000042"
+
+# Flexible mount directory for testing
+[ -z ${BOOTENGINE_ROOT_DIR} ] && BOOTENGINE_ROOT_DIR=/sysroot
+BOOTENGINE_USR_DIR=${BOOTENGINE_ROOT_DIR}/usr
+MACHINE_ID_FILE="${BOOTENGINE_ROOT_DIR}/etc/machine-id"
+
+# Run and log a command
+bootengine_cmd() {
+    ret=0
+    "$@" >/tmp/bootengine.out 2>&1 || ret=$?
+    vinfo < /tmp/bootengine.out
+    if [ $ret -ne 0 ]; then
+        warn "bootengine: command failed: $*"
+        warn "bootengine: command returned $ret"
+    fi
+    return $ret
+}
+
+do_setup_root() {
+    if ! bootengine_cmd mount -o remount,rw "${BOOTENGINE_ROOT_DIR}"; then
+        warn "bootengine setup root: Can't remount root rw"
+        return 0
+    fi
+
+    # Initialize base filesystem
+    bootengine_cmd systemd-tmpfiles \
+        --root="${BOOTENGINE_ROOT_DIR}" --create \
+        baselayout.conf baselayout-etc.conf baselayout-usr.conf
+
+    # Check for "initial" /etc/machine-id or a blank / non-existant
+    # /etc/machine-id file and create a "real" one instead.
+    if grep -q '^[0-9a-fA-F]{32}$' "${MACHINE_ID_FILE}" && \
+        [ "$(cat "${MACHINE_ID_FILE}")" != "${COREOS_BLANK_MACHINE_ID}" ] ; then
+        info "bootengine: machine-id is valid"
+    else
+        info "bootengine: generating new machine-id"
+        rm -f "${MACHINE_ID_FILE}"
+        bootengine_cmd systemd-machine-id-setup --root="${BOOTENGINE_ROOT_DIR}"
+    fi
+}
+
+# Skip if root and root/usr are not mount points
+if ismounted "${BOOTENGINE_ROOT_DIR}" && \
+   ismounted "${BOOTENGINE_USR_DIR}"; then
+    do_setup_root
+fi
