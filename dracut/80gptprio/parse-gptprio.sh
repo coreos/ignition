880c3c1a1d0fad40b7e4b49027b4f405cafc6701
Michael Marineau <michael.marineau@coreos.com>
2014-03-25 16:44:04 -0700

    add(10usr-generator): Move usr=/dev/foo handling to a systemd generator
    The dracut parse hooks may not run early enough for
    systemd-fstab-generator to catch what gets written to fstab at that
    point. This means that /usr may not get mounted before systemd reaches
    the initrd-root-fs.target and starts to switch the new root. This
    manifests itself as random hangs during boot or the machine-id not being
    properly initialized on first boot. Creating our own generator that
    works like systemd-fstab-generator should fix this issue.
    
    It would be nice to move this functionality into systemd itself.

--- dracut/80gptprio/parse-gptprio.sh
+++ dracut/80gptprio/parse-gptprio.sh
@@ -4,33 +4,6 @@
 
 export usr="$(getarg usr=)"
 
-case "$usr" in
-    block:LABEL=*|LABEL=*)
-        usr="${usr#block:}"
-        usr="$(echo $usr | sed 's,/,\\x2f,g')"
-        usr="block:/dev/disk/by-label/${usr#LABEL=}"
-        ;;
-    block:UUID=*|UUID=*)
-        usr="${usr#block:}"
-        usr="${usr#UUID=}"
-        usr="$(echo $usr | tr "[:upper:]" "[:lower:]")"
-        usr="block:/dev/disk/by-uuid/${usr#UUID=}"
-        ;;
-    block:PARTUUID=*|PARTUUID=*)
-        usr="${usr#block:}"
-        usr="${usr#PARTUUID=}"
-        usr="$(echo $usr | tr "[:upper:]" "[:lower:]")"
-        usr="block:/dev/disk/by-partuuid/${usr}"
-        ;;
-    block:PARTLABEL=*|PARTLABEL=*)
-        usr="${usr#block:}"
-        usr="block:/dev/disk/by-partlabel/${usr#PARTLABEL=}"
-        ;;
-    /dev/*)
-        usr="block:${usr}"
-        ;;
-esac
-
 if [ "${usr%%:*}" = "gptprio" -a "${root%%:*}" = "gptprio" ]; then
     die "Cannot use both root and usr gptprio"
 
@@ -42,12 +15,4 @@ elif [ "${usr%%:*}" = "gptprio" -o "${root%%:*}" = "gptprio" ]; then
     # Wait for both partitions to show up before we move on
     wait_for_dev "/dev/disk/by-partuuid/7130c94a-213a-4e5a-8e26-6cce9662f132"
     wait_for_dev "/dev/disk/by-partuuid/e03dd35c-7c2d-4a47-b3fe-27f15780a57c"
-
-elif [ "${usr%%:*}" = "block" ]; then
-    usrflags="$(getarg usrflags=)"
-    usrfstype="$(getarg usrfstype=)"
-
-    # Add basic block devices to the initrd's fstab
-    add_mount_point "${usr#block:}" /sysroot/usr \
-        "${usrfstype:-auto}" "${usrflags:-ro},x-initrd.mount"
 fi
