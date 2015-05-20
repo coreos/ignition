709eed7367773cb7546b38aee4d1f62b629d0b3e
Alex Crawford <alex.crawford@coreos.com>
2015-05-20 12:01:44 -0700

    stages/storage: actually refer to the stage as "storage"

--- src/exec/stages/storage/storage.go
+++ src/exec/stages/storage/storage.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package disks
+package storage
 
 import (
 	"fmt"
@@ -30,7 +30,7 @@ import (
 )
 
 const (
-	name = "disks"
+	name = "storage"
 )
 
 func init() {
