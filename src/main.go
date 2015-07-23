8c900a1ef662500478cab8fe723bc93481203777
Alex Crawford <alex.crawford@coreos.com>
2015-07-22 17:09:30 -0700

    version: bump to 0.1.1

--- src/main.go
+++ src/main.go
@@ -35,7 +35,7 @@ import (
 	"github.com/coreos/ignition/third_party/github.com/coreos/go-semver/semver"
 )
 
-const versionString = "0.1.0+git"
+const versionString = "0.1.1"
 
 var version = *semver.Must(semver.NewVersion(versionString))
 
