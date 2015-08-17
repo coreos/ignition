82e935313174c574743fd209790635abed8aa774
Alex Crawford <alex.crawford@coreos.com>
2015-08-17 12:01:07 -0700

    dracut: add ignition systemd generator
    This generator looks for the flag "coreos.ignition.force" and will
    enable ignition.service unless it's a false value (0, no, off, ).

--- dracut/30ignition/module-setup.sh
+++ dracut/30ignition/module-setup.sh
@@ -2,6 +2,10 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
+depends() {
+    echo systemd
+}
+
 install() {
     inst_multiple \
         ignition \
@@ -13,4 +17,7 @@ install() {
         "$systemdsystemunitdir/ignition.target" \
         "$systemdsystemunitdir/ignition-disks.service" \
         "$systemdsystemunitdir/ignition-files.service"
+
+    inst_simple "$moddir/ignition-generator" \
+        "$systemdutildir/system-generators/ignition-generator"
 }
