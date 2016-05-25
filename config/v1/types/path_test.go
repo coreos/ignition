76ee57d6aeb0e1c6a3dbb21d2028a53da0660d32
Alex Crawford <alex.crawford@coreos.com>
2016-05-25 13:57:03 -0700

    config/v1/types: remove support for yaml

--- config/v1/types/path_test.go
+++ config/v1/types/path_test.go
@@ -18,8 +18,6 @@ import (
 	"encoding/json"
 	"reflect"
 	"testing"
-
-	"github.com/go-yaml/yaml"
 )
 
 func TestPathUnmarshalJSON(t *testing.T) {
@@ -48,41 +46,6 @@ func TestPathUnmarshalJSON(t *testing.T) {
 	for i, test := range tests {
 		var device Path
 		err := json.Unmarshal([]byte(test.in.data), &device)
-		if !reflect.DeepEqual(test.out.err, err) {
-			t.Errorf("#%d: bad error: want %v, got %v", i, test.out.err, err)
-		}
-		if !reflect.DeepEqual(test.out.device, device) {
-			t.Errorf("#%d: bad device: want %#v, got %#v", i, test.out.device, device)
-		}
-	}
-}
-
-func TestPathUnmarshalYAML(t *testing.T) {
-	type in struct {
-		data string
-	}
-	type out struct {
-		device Path
-		err    error
-	}
-
-	tests := []struct {
-		in  in
-		out out
-	}{
-		{
-			in:  in{data: `"/path"`},
-			out: out{device: Path("/path")},
-		},
-		{
-			in:  in{data: `"bad"`},
-			out: out{device: Path("bad"), err: ErrPathRelative},
-		},
-	}
-
-	for i, test := range tests {
-		var device Path
-		err := yaml.Unmarshal([]byte(test.in.data), &device)
 		if !reflect.DeepEqual(test.out.err, err) {
 			t.Errorf("#%d: bad error: want %v, got %v", i, test.out.err, err)
 		}
