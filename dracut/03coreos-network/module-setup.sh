7b3308be7e590553f688e272e1ffa64a553eaa37
David Michael <david.michael@coreos.com>
2016-07-29 12:04:09 -0700

    dracut: build onto the upstream systemd-networkd module
    This drops both the systemd-networkd and kernel-network-modules
    directories from bootengine's dracut modules, since they are both
    available from the updated dracut version.  Instead, a new module,
    coreos-network, has been added to install systemd-resolved and a
    few configuration changes that used to be in our systemd-networkd.

---
+++ dracut/03coreos-network/module-setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This module extends dracut's systemd-networkd module to include additional
+# networking configuration for Ignition.
+
+# called by dracut
+depends() {
+    echo systemd-networkd
+}
+
+# called by dracut
+install() {
+    inst_multiple -o \
+        $systemdutildir/systemd-resolved \
+        $systemdsystemunitdir/systemd-resolved.service \
+        /etc/systemd/resolved.conf
+
+    inst_simple "$moddir/10-down.conf" \
+        "$systemdsystemunitdir/systemd-networkd.service.d/10-down.conf"
+
+    inst_simple "$moddir/10-nodeps.conf" \
+        "$systemdsystemunitdir/systemd-resolved.service.d/10-nodeps.conf"
+
+    inst_simple "$moddir/yy-pxe.network" \
+        "$systemdutildir/network/yy-pxe.network"
+
+    inst_simple "$moddir/zz-default.network" \
+        "$systemdutildir/network/zz-default.network"
+
+    # user/group required for systemd-resolved
+    getent passwd systemd-resolve >> "$initdir/etc/passwd"
+    getent group systemd-resolve >> "$initdir/etc/group"
+
+    # point /etc/resolv.conf @ systemd-resolved's resolv.conf
+    ln -s ../run/systemd/resolve/resolv.conf "$initdir/etc/resolv.conf"
+}
