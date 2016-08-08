9ac00e14941000c499aee08972a592efb2b6c6ab
Andrew Jeddeloh <andrew.jeddeloh@coreos.com>
2016-08-08 11:57:04 -0700

    tests: update tests to not use unmarshallers

--- config/types/path_test.go
+++ config/types/path_test.go
@@ -15,45 +15,9 @@
 package types
 
 import (
-	"encoding/json"
 	"reflect"
 	"testing"
 )
-
-func TestPathUnmarshalJSON(t *testing.T) {
-	type in struct {
-		data string
-	}
-	type out struct {
-		device Path
-		err    error
-	}
-
-	tests := []struct {
-		in  in
-		out out
-	}{
-		{
-			in:  in{data: `"/path"`},
-			out: out{device: Path("/path")},
-		},
-		{
-			in:  in{data: `"bad"`},
-			out: out{device: Path("bad"), err: ErrPathRelative},
-		},
-	}
-
-	for i, test := range tests {
-		var device Path
-		err := json.Unmarshal([]byte(test.in.data), &device)
-		if !reflect.DeepEqual(test.out.err, err) {
-			t.Errorf("#%d: bad error: want %v, got %v", i, test.out.err, err)
-		}
-		if !reflect.DeepEqual(test.out.device, device) {
-			t.Errorf("#%d: bad device: want %#v, got %#v", i, test.out.device, device)
-		}
-	}
-}
 
 func TestPathAssertValid(t *testing.T) {
 	type in struct {
--- config/types/unit_test.go
+++ config/types/unit_test.go
@@ -15,19 +15,17 @@
 package types
 
 import (
-	"encoding/json"
 	"errors"
 	"reflect"
 	"testing"
 )
 
-func TestSystemdUnitNameUnmarshalJSON(t *testing.T) {
+func TestSystemdUnitNameAssertValid(t *testing.T) {
 	type in struct {
-		data string
+		unit SystemdUnitName
 	}
 	type out struct {
-		unit SystemdUnitName
-		err  error
+		err error
 	}
 
 	tests := []struct {
@@ -35,42 +33,33 @@ func TestSystemdUnitNameUnmarshalJSON(t *testing.T) {
 		out out
 	}{
 		{
-			in:  in{data: `"test.service"`},
-			out: out{unit: SystemdUnitName("test.service")},
+			in:  in{unit: SystemdUnitName("test.service")},
+			out: out{err: nil},
 		},
 		{
-			in:  in{data: `"test.socket"`},
-			out: out{unit: SystemdUnitName("test.socket")},
+			in:  in{unit: SystemdUnitName("test.socket")},
+			out: out{err: nil},
 		},
 		{
-			in:  in{data: `"test.blah"`},
+			in:  in{unit: SystemdUnitName("test.blah")},
 			out: out{err: errors.New("invalid systemd unit extension")},
 		},
 	}
 
 	for i, test := range tests {
-		var unit SystemdUnitName
-		err := json.Unmarshal([]byte(test.in.data), &unit)
+		err := test.in.unit.AssertValid()
 		if !reflect.DeepEqual(test.out.err, err) {
 			t.Errorf("#%d: bad error: want %v, got %v", i, test.out.err, err)
-		}
-		if err != nil {
-			continue
-		}
-
-		if !reflect.DeepEqual(test.out.unit, unit) {
-			t.Errorf("#%d: bad unit: want %#v, got %#v", i, test.out.unit, unit)
 		}
 	}
 }
 
-func TestNetworkdUnitNameUnmarshalJSON(t *testing.T) {
+func TestNetworkdUnitNameAssertValid(t *testing.T) {
 	type in struct {
-		data string
+		unit NetworkdUnitName
 	}
 	type out struct {
-		unit NetworkdUnitName
-		err  error
+		err error
 	}
 
 	tests := []struct {
@@ -78,35 +67,27 @@ func TestNetworkdUnitNameUnmarshalJSON(t *testing.T) {
 		out out
 	}{
 		{
-			in:  in{data: `"test.network"`},
-			out: out{unit: NetworkdUnitName("test.network")},
+			in:  in{unit: NetworkdUnitName("test.network")},
+			out: out{err: nil},
 		},
 		{
-			in:  in{data: `"test.link"`},
-			out: out{unit: NetworkdUnitName("test.link")},
+			in:  in{unit: NetworkdUnitName("test.link")},
+			out: out{err: nil},
 		},
 		{
-			in:  in{data: `"test.netdev"`},
-			out: out{unit: NetworkdUnitName("test.netdev")},
+			in:  in{unit: NetworkdUnitName("test.netdev")},
+			out: out{err: nil},
 		},
 		{
-			in:  in{data: `"test.blah"`},
+			in:  in{unit: NetworkdUnitName("test.blah")},
 			out: out{err: errors.New("invalid networkd unit extension")},
 		},
 	}
 
 	for i, test := range tests {
-		var unit NetworkdUnitName
-		err := json.Unmarshal([]byte(test.in.data), &unit)
+		err := test.in.unit.AssertValid()
 		if !reflect.DeepEqual(test.out.err, err) {
 			t.Errorf("#%d: bad error: want %v, got %v", i, test.out.err, err)
-		}
-		if err != nil {
-			continue
-		}
-
-		if !reflect.DeepEqual(test.out.unit, unit) {
-			t.Errorf("#%d: bad unit: want %#v, got %#v", i, test.out.unit, unit)
 		}
 	}
 }
