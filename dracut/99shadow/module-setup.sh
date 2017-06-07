9dc0e7e9f539a6ff7b31e5a5aaf7c28258a8e176
Derek Gonyeo <derek@gonyeo.com>
2017-06-07 16:19:31 -0700

    Revert "add sysusers to the initramfs"
    This reverts commit cda17918fdb28e9e3fc14f5cae6d6f20f75f6a2e.

--- dracut/99shadow/module-setup.sh
+++ dracut/99shadow/module-setup.sh
@@ -3,10 +3,17 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
 install() {
-    # Run systemd-sysusers during the build so things like systemd-tmpfiles
+    # Simply pull in all the shadow db files so things like systemd-tmpfiles
     # will always be able to find users referenced by the baselayout files.
-    cp -ar "/usr/lib/sysusers.d" \
-        "${initdir}/usr/lib/"
+    cp -af "/usr/share/baselayout/passwd" \
+        "${initdir}/etc/passwd"
 
-    systemd-sysusers --root="${initdir}"
+    cp -af "/usr/share/baselayout/shadow" \
+        "${initdir}/etc/shadow"
+
+    cp -af "/usr/share/baselayout/group" \
+        "${initdir}/etc/group"
+
+    cp -af "/usr/share/baselayout/gshadow" \
+        "${initdir}/etc/gshadow"
 }
