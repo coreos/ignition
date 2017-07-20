coreos/ignition

commit bb701519bafa29911b80c02cfd3a25ac3e428d38

*: add support for reusing existing filesystems
This adds the 'wipeFilesystem' flag to filesystem's 'mount' object. When
this flag is false, Ignition will inspect the current filesystem's type,
and reuse it if it matches the desired state. If this flag is true, the
current filesystem will always be reformatted when Ignition is run.
This is useful if the user wants to persist data across multiple runs of
Ignition.

This new flag can be safely used in conjunction with the deprecated
'force' flag.  When both are set, the target filesystem is first checked
to see if it's the correct type. If so, the format operation is skipped.
If not, the 'force' flag determines if the existing filesystem (if any)
will be overwritten.

internal/exec/stages/disks/disks.go
@@ -285,7 +285,12 @@ func (s stage) createFilesystems(config types.Config) error {
 }
 
 func (s stage) createFilesystem(fs types.Mount) error {
-	if fs.Create == nil {
+	format, err := s.readFilesystemType(fs)
+	if err != nil {
+		return err
+	}
+	if format == fs.Format && !fs.WipeFilesystem {
+		s.Logger.Info("filesystem at %q is already formatted. Skipping mkfs...", fs.Device)
 		return nil
 	}
 
@@ -293,6 +298,7 @@ func (s stage) createFilesystem(fs types.Mount) error {
 	var force bool
 	var args []string
 	if fs.Create == nil {
+		force = fs.WipeFilesystem
 		args = translateMountOptionSliceToStringSlice(fs.Options)
 	} else {
 		force = fs.Create.Force
@@ -353,4 +359,20 @@ func translateCreateOptionSliceToStringSlice(opts []types.CreateOption) []string
 		newOpts[i] = string(o)
 	}
 	return newOpts
+}
+
+func (s stage) readFilesystemType(fs types.Mount) (string, error) {
+	var fsType string
+	err := s.Logger.LogOp(
+		func() (err error) {
+			fsType, err = util.FilesystemType(fs.Device)
+			if err == nil {
+				s.Logger.Info("found %s filesystem at %q", fsType, fs.Device)
+			}
+			return
+		},
+		"determining filesystem type of %q", fs.Device,
+	)
+
+	return fsType, err
 }

.travis.yml
@@ -21,14 +21,25 @@ addons:
     # install cross compilers for cgo support.
     - gcc-aarch64-linux-gnu
     - libc6-dev-arm64-cross
+    - libblkid-dev
 
 install:
-  -
+  # since libblkid-dev:arm64 cannot be installed, spoof it
+  -     if [ "${TARGET}" == "arm64" ]; then
+                echo "void blkid_new_probe_from_filename(void) {}" >> stub.c;
+                echo "void blkid_do_probe(void) {}"                >> stub.c;
+                echo "void blkid_probe_has_value(void) {}"         >> stub.c;
+                echo "void blkid_probe_lookup_value(void) {}"      >> stub.c;
+                echo "void blkid_free_probe(void) {}"              >> stub.c;
+                aarch64-linux-gnu-gcc-4.8 -c -o stub.o stub.c;
+                aarch64-linux-gnu-gcc-ar-4.8 cq libblkid.a stub.o;
+        fi
 
 script:
   -     if [ "${TARGET}" == "amd64" ]; then
                 GOARCH="${TARGET}" ./test;
         elif [ "${TARGET}" == "arm64" ]; then
+                export CGO_LDFLAGS="-L ${PWD}";
                 GOARCH="${TARGET}" ./build;
                 file "bin/${TARGET}/ignition" | egrep 'aarch64';
         fi

config/types/filesystem.go
@@ -22,11 +22,12 @@ import (
 )
 
 var (
-	ErrFilesystemInvalidFormat = errors.New("invalid filesystem format")
-	ErrFilesystemNoMountPath   = errors.New("filesystem is missing mount or path")
-	ErrFilesystemMountAndPath  = errors.New("filesystem has both mount and path defined")
-	ErrUsedCreateAndMountOpts  = errors.New("cannot use both create object and mount-level options fields")
-	ErrWarningCreateDeprecated = errors.New("the create object has been deprecated in favor of mount-level options")
+	ErrFilesystemInvalidFormat     = errors.New("invalid filesystem format")
+	ErrFilesystemNoMountPath       = errors.New("filesystem is missing mount or path")
+	ErrFilesystemMountAndPath      = errors.New("filesystem has both mount and path defined")
+	ErrUsedCreateAndMountOpts      = errors.New("cannot use both create object and mount-level options field")
+	ErrUsedCreateAndWipeFilesystem = errors.New("cannot use both create object and wipeFilesystem field")
+	ErrWarningCreateDeprecated     = errors.New("the create object has been deprecated in favor of mount-level options")
 )
 
 func (f Filesystem) Validate() report.Report {
@@ -45,6 +46,12 @@ func (f Filesystem) Validate() report.Report {
 			})
 		}
 		if f.Mount.Create != nil {
+			if f.Mount.WipeFilesystem {
+				r.Add(report.Entry{
+					Message: ErrUsedCreateAndWipeFilesystem.Error(),
+					Kind:    report.EntryError,
+				})
+			}
 			if len(f.Mount.Options) > 0 {
 				r.Add(report.Entry{
 					Message: ErrUsedCreateAndMountOpts.Error(),

config/types/schema.go
@@ -88,10 +88,11 @@ type LinkEmbedded1 struct {
 }
 
 type Mount struct {
-	Create  *Create       `json:"create,omitempty"`
-	Device  string        `json:"device,omitempty"`
-	Format  string        `json:"format,omitempty"`
-	Options []MountOption `json:"options,omitempty"`
+	Create         *Create       `json:"create,omitempty"`
+	Device         string        `json:"device,omitempty"`
+	Format         string        `json:"format,omitempty"`
+	Options        []MountOption `json:"options,omitempty"`
+	WipeFilesystem bool          `json:"wipeFilesystem,omitempty"`
 }
 
 type MountOption string

internal/exec/util/blkid.c
@@ -1,0 +1,39 @@
+// Copyright 2016 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <blkid/blkid.h>
+#include "blkid.h"
+
+result_t filesystem_type(const char *device, char type[], size_t type_len)
+{
+	const char *type_name = "\0";
+
+	blkid_probe pr = blkid_new_probe_from_filename(device);
+	if (!pr)
+		return RESULT_OPEN_FAILED;
+
+	if (blkid_do_probe(pr) != 0)
+		return RESULT_PROBE_FAILED;
+
+	if (blkid_probe_has_value(pr, "TYPE"))
+		if (blkid_probe_lookup_value(pr, "TYPE", &type_name, NULL))
+			return RESULT_LOOKUP_FAILED;
+
+	strncpy(type, type_name, type_len);
+
+	blkid_free_probe(pr);
+
+	return RESULT_OK;
+}
+

internal/exec/util/blkid.go
@@ -1,0 +1,57 @@
+// Copyright 2015 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build linux
+
+package util
+
+// #cgo LDFLAGS: -lblkid
+// #include <stdlib.h>
+// #include "blkid.h"
+import "C"
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func FilesystemType(device string) (string, error) {
+	var fsType [16]byte
+
+	cDevice := C.CString(device)
+	defer C.free(unsafe.Pointer(cDevice))
+
+	switch C.filesystem_type(cDevice, (*C.char)(unsafe.Pointer(&fsType[0])), C.size_t(len(fsType))) {
+	case C.RESULT_OK:
+		return string(trimZerosTail(fsType[:])), nil
+	case C.RESULT_OPEN_FAILED:
+		return "", fmt.Errorf("failed to open %q", device)
+	case C.RESULT_PROBE_FAILED:
+		// If the probe failed, there's no filesystem created yet on this device
+		return "", nil
+	case C.RESULT_LOOKUP_FAILED:
+		return "", fmt.Errorf("failed to lookup filesystem type of %q", device)
+	default:
+		return "", fmt.Errorf("unknown error")
+	}
+}
+
+func trimZerosTail(b []byte) []byte {
+	for i := range b {
+		if b[i] == 0 {
+			return b[:i]
+		}
+	}
+	return b
+}

internal/exec/util/blkid.h
@@ -1,0 +1,30 @@
+// Copyright 2016 CoreOS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef _BLKID_H_
+#define _BLKID_H_
+
+#include <string.h>
+
+typedef enum {
+	RESULT_OK,
+	RESULT_OPEN_FAILED,
+	RESULT_PROBE_FAILED,
+	RESULT_LOOKUP_FAILED,
+} result_t;
+
+result_t filesystem_type(const char *device, char type[], size_t type_len);
+
+#endif // _BLKID_H_
+

commit e2e8f48246bb2fe55d27598f53916f36afc35195

stages/disks: fix handling of legacy force flag
This was broken by bb701519b. Both the new WipeFilesystems flag and the
legacy Force flag need to be checked.

internal/exec/stages/disks/disks.go
@@ -292,6 +292,7 @@ func (s stage) createFilesystem(fs types.Mount) error {
 	if info.format == fs.Format &&
 		(fs.Label == nil || info.label == *fs.Label) &&
 		(fs.UUID == nil || info.uuid == *fs.UUID) &&
+		(fs.Create == nil || !fs.Create.Force) &&
 		!fs.WipeFilesystem {
 		s.Logger.Info("filesystem at %q is already formatted. Skipping mkfs...", fs.Device)
 		return nil