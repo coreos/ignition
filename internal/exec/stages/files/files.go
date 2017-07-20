coreos/ignition

commit 8f6e802753404e5bac805cb46054243fe72005ab

internal/resource: refactor to take writers instead of produce readers
This commit refactors internal/resource/url to ingest an *os.File
instead of produce an io.Reader, in addition to general cleanup. There
were a few motivators for this change:

- A followup commit will add s3 support, and the s3 library works best
 (i.e. uses chunked downloads) when given an io.WriterAt. With these
 changes, this will now be easy
- Transmission related logic like hash calculation and decompression now
  all live in the same place as the code doing the fetching
- This file was generally in need of cleanup, with code smells like
  generic fetch functions taking http specific arguments

This change resulted in a few cascading effects throughout the codebase:

- RenderFile in internal/exec/util/files.go no longer initiates the
  fetch, the produced struct now only contains a URL to use when
  fetching the data
- WriteFile in internal/exec/util/files.go now uses the URL recorded in
  the struct to call Fetch directly
- internal/exec/util/files.go no longer has any decompression logic
- internal/exec/util/files.go no longer has any hashing logic
- internal/exec/util/unit.go now stores unit files as dataurl encoded
  strings, instead of in an internal buffer to produce an io.Reader

internal/exec/stages/files/files.go
@@ -45,13 +45,13 @@ func init() {
 
 type creator struct{}
 
-func (creator) Create(logger *log.Logger, client *resource.HttpClient, root string) stages.Stage {
+func (creator) Create(logger *log.Logger, root string, f resource.Fetcher) stages.Stage {
 	return &stage{
 		Util: util.Util{
 			DestDir: root,
 			Logger:  logger,
+			Fetcher: f,
 		},
-		client: client,
 	}
 }
 
@@ -127,16 +127,16 @@ func (tmp fileEntry) create(l *log.Logger, c *resource.HttpClient, u util.Util) 
 		f.Group.ID = internalUtil.IntToPtr(0)
 	}
 
-	file := util.RenderFile(l, c, f)
-	if file == nil {
+	fetchOp := u.PrepareFetch(l, c, f)
+	if fetchOp == nil {
 		return fmt.Errorf("failed to resolve file %q", f.Path)
 	}
 
 	if err := l.LogOp(
-		func() error { return u.WriteFile(file) },
+		func() error { return u.PerformFetch(fetchOp) },
 		"writing file %q", string(f.Path),
 	); err != nil {
-		return fmt.Errorf("failed to create file %q: %v", file.Path, err)
+		return fmt.Errorf("failed to create file %q: %v", fetchOp.Path, err)
 	}
 
 	return nil
@@ -305,13 +305,8 @@ func (s stage) createEntries(fs types.Filesystem, files []filesystemEntry) error
 		mnt = *fs.Path
 	}
 
-	u := util.Util{
-		Logger:  s.Logger,
-		DestDir: mnt,
-	}
-
 	for _, e := range files {
-		if err := e.create(s.Logger, s.client, u); err != nil {
+		if err := e.create(s.Logger, s.client, s.Util); err != nil {
 			return err
 		}
 	}
@@ -360,9 +355,13 @@ func (s stage) writeSystemdUnit(unit types.Unit) error {
 				continue
 			}
 
-			f := util.FileFromUnitDropin(unit, dropin)
+			f, err := util.FileFromUnitDropin(unit, dropin)
+			if err != nil {
+				s.Logger.Crit("error converting dropin: %v", err)
+				return err
+			}
 			if err := s.Logger.LogOp(
-				func() error { return s.WriteFile(f) },
+				func() error { return s.PerformFetch(f) },
 				"writing drop-in %q at %q", dropin.Name, f.Path,
 			); err != nil {
 				return err
@@ -373,9 +372,13 @@ func (s stage) writeSystemdUnit(unit types.Unit) error {
 			return nil
 		}
 
-		f := util.FileFromSystemdUnit(unit)
+		f, err := util.FileFromSystemdUnit(unit)
+		if err != nil {
+			s.Logger.Crit("error converting unit: %v", err)
+			return err
+		}
 		if err := s.Logger.LogOp(
-			func() error { return s.WriteFile(f) },
+			func() error { return s.PerformFetch(f) },
 			"writing unit %q at %q", unit.Name, f.Path,
 		); err != nil {
 			return err
@@ -393,9 +396,13 @@ func (s stage) writeNetworkdUnit(unit types.Networkdunit) error {
 			return nil
 		}
 
-		f := util.FileFromNetworkdUnit(unit)
+		f, err := util.FileFromNetworkdUnit(unit)
+		if err != nil {
+			s.Logger.Crit("error converting unit: %v", err)
+			return err
+		}
 		if err := s.Logger.LogOp(
-			func() error { return s.WriteFile(f) },
+			func() error { return s.PerformFetch(f) },
 			"writing unit %q at %q", unit.Name, f.Path,
 		); err != nil {
 			return err

internal/exec/engine.go
@@ -26,6 +26,7 @@ import (
 	"github.com/coreos/ignition/internal/exec/stages"
 	"github.com/coreos/ignition/internal/exec/util"
 	"github.com/coreos/ignition/internal/log"
+	"github.com/coreos/ignition/internal/oem"
 	"github.com/coreos/ignition/internal/providers"
 	"github.com/coreos/ignition/internal/providers/cmdline"
 	"github.com/coreos/ignition/internal/resource"
@@ -49,13 +50,11 @@ var (
 
 // Engine represents the entity that fetches and executes a configuration.
 type Engine struct {
-	ConfigCache       string
-	FetchTimeout      time.Duration
-	Logger            *log.Logger
-	Root              string
-	FetchFunc         providers.FuncFetchConfig
-	OemBaseConfig     types.Config
-	DefaultUserConfig types.Config
+	ConfigCache  string
+	FetchTimeout time.Duration
+	Logger       *log.Logger
+	Root         string
+	OEMConfig    oem.Config
 
 	client resource.HttpClient
 }
@@ -63,12 +62,12 @@ type Engine struct {
 // Run executes the stage of the given name. It returns true if the stage
 // successfully ran and false if there were any errors.
 func (e Engine) Run(stageName string) bool {
-	cfg, err := e.acquireConfig()
+	cfg, f, err := e.acquireConfig()
 	switch err {
 	case nil:
 	case config.ErrCloudConfig, config.ErrScript, config.ErrEmpty:
 		e.Logger.Info("%v: ignoring user-provided config", err)
-		cfg = e.DefaultUserConfig
+		cfg = e.OEMConfig.DefaultUserConfig()
 	default:
 		e.Logger.Crit("failed to acquire config: %v", err)
 		return false
@@ -76,29 +75,52 @@ func (e Engine) Run(stageName string) bool {
 
 	e.Logger.PushPrefix(stageName)
 	defer e.Logger.PopPrefix()
-	return stages.Get(stageName).Create(e.Logger, &e.client, e.Root).Run(config.Append(baseConfig, config.Append(e.OemBaseConfig, cfg)))
+	return stages.Get(stageName).Create(e.Logger, e.Root, f).Run(config.Append(baseConfig, config.Append(e.OEMConfig.BaseConfig(), cfg)))
 }
 
 // acquireConfig returns the configuration, first checking a local cache
 // before attempting to fetch it from the provider.
-func (e *Engine) acquireConfig() (cfg types.Config, err error) {
+func (e *Engine) acquireConfig() (cfg types.Config, f resource.Fetcher, err error) {
 	// First try read the config @ e.ConfigCache.
 	b, err := ioutil.ReadFile(e.ConfigCache)
 	if err == nil {
 		if err = json.Unmarshal(b, &cfg); err != nil {
 			e.Logger.Crit("failed to parse cached config: %v", err)
 		}
+		// Create an http client and fetcher with the timeouts from the cached
+		// config
 		e.client = resource.NewHttpClient(e.Logger, cfg.Ignition.Timeouts)
+		f, err = e.OEMConfig.NewFetcherFunc()(e.Logger, &e.client)
+		if err != nil {
+			e.Logger.Crit("failed to generate fetcher: %s", err)
+			return
+		}
 		return
 	}
 
+	// Create a new http client and fetcher with the timeouts set via the flags,
+	// since we don't have a config with timeout values we can use
 	timeout := int(e.FetchTimeout.Seconds())
 	e.client = resource.NewHttpClient(e.Logger, types.Timeouts{HTTPTotal: &timeout})
+	f, err = e.OEMConfig.NewFetcherFunc()(e.Logger, &e.client)
+	if err != nil {
+		e.Logger.Crit("failed to generate fetcher: %s", err)
+		return
+	}
 
 	// (Re)Fetch the config if the cache is unreadable.
-	cfg, err = e.fetchProviderConfig()
+	cfg, err = e.fetchProviderConfig(f)
 	if err != nil {
 		e.Logger.Crit("failed to fetch config: %s", err)
+		return
+	}
+
+	// Regenerate the http client and fetcher to use the timeouts from the
+	// newly fetched config
+	e.client = resource.NewHttpClient(e.Logger, cfg.Ignition.Timeouts)
+	f, err = e.OEMConfig.NewFetcherFunc()(e.Logger, &e.client)
+	if err != nil {
+		e.Logger.Crit("failed to generate fetcher: %s", err)
 		return
 	}
 
@@ -122,10 +144,10 @@ func (e *Engine) acquireConfig() (cfg types.Config, err error) {
 // check's the engine's provider. An error is returned if the provider is
 // unavailable. This will also render the config (see renderConfig) before
 // returning.
-func (e Engine) fetchProviderConfig() (types.Config, error) {
-	cfg, r, err := cmdline.FetchConfig(e.Logger, &e.client)
+func (e Engine) fetchProviderConfig(f resource.Fetcher) (types.Config, error) {
+	cfg, r, err := cmdline.FetchConfig(f)
 	if err == providers.ErrNoProvider {
-		cfg, r, err = e.FetchFunc(e.Logger, &e.client)
+		cfg, r, err = e.OEMConfig.FetchFunc()(f)
 	}
 
 	e.logReport(r)
@@ -133,7 +155,7 @@ func (e Engine) fetchProviderConfig() (types.Config, error) {
 		return types.Config{}, err
 	}
 
-	return e.renderConfig(cfg)
+	return e.renderConfig(cfg, f)
 }
 
 // renderConfig evaluates "ignition.config.replace" and "ignition.config.append"
@@ -142,16 +164,16 @@ func (e Engine) fetchProviderConfig() (types.Config, error) {
 // "ignition.config.append" is set, each of the referenced configs will be
 // evaluated and appended to the provided config. If neither option is set, the
 // provided config will be returned unmodified.
-func (e *Engine) renderConfig(cfg types.Config) (types.Config, error) {
+func (e *Engine) renderConfig(cfg types.Config, f resource.Fetcher) (types.Config, error) {
 	// Apply any new timeout info before fetching other configs.
 	e.client = resource.NewHttpClient(e.Logger, cfg.Ignition.Timeouts)
 	if cfgRef := cfg.Ignition.Config.Replace; cfgRef != nil {
-		return e.fetchReferencedConfig(*cfgRef)
+		return e.fetchReferencedConfig(*cfgRef, f)
 	}
 
 	appendedCfg := cfg
 	for _, cfgRef := range cfg.Ignition.Config.Append {
-		newCfg, err := e.fetchReferencedConfig(cfgRef)
+		newCfg, err := e.fetchReferencedConfig(cfgRef, f)
 		if err != nil {
 			return newCfg, err
 		}
@@ -163,12 +185,14 @@ func (e *Engine) renderConfig(cfg types.Config) (types.Config, error) {
 
 // fetchReferencedConfig fetches, renders, and attempts to verify the requested
 // config.
-func (e Engine) fetchReferencedConfig(cfgRef types.ConfigReference) (types.Config, error) {
+func (e Engine) fetchReferencedConfig(cfgRef types.ConfigReference, f resource.Fetcher) (types.Config, error) {
 	u, err := url.Parse(cfgRef.Source)
 	if err != nil {
 		return types.Config{}, err
 	}
-	rawCfg, err := resource.Fetch(e.Logger, &e.client, *u)
+	rawCfg, err := f.FetchToBuffer(*u, resource.FetchOptions{
+		Headers: resource.ConfigHeaders,
+	})
 	if err != nil {
 		return types.Config{}, err
 	}
@@ -183,7 +207,7 @@ func (e Engine) fetchReferencedConfig(cfgRef types.ConfigReference) (types.Confi
 		return types.Config{}, err
 	}
 
-	return e.renderConfig(cfg)
+	return e.renderConfig(cfg, f)
 }
 
 func (e Engine) logReport(r report.Report) {

internal/exec/stages/disks/disks.go
@@ -41,13 +41,13 @@ func init() {
 
 type creator struct{}
 
-func (creator) Create(logger *log.Logger, client *resource.HttpClient, root string) stages.Stage {
+func (creator) Create(logger *log.Logger, root string, f resource.Fetcher) stages.Stage {
 	return &stage{
 		Util: util.Util{
 			DestDir: root,
 			Logger:  logger,
+			Fetcher: f,
 		},
-		client: client,
 	}
 }
 

internal/exec/stages/stages.go
@@ -30,7 +30,7 @@ type Stage interface {
 // StageCreator is responsible for instantiating a particular stage given a
 // logger and root path under the root partition.
 type StageCreator interface {
-	Create(logger *log.Logger, client *resource.HttpClient, root string) Stage
+	Create(logger *log.Logger, root string, f resource.Fetcher) Stage
 	Name() string
 }
 

internal/exec/util/file.go
@@ -15,8 +15,6 @@
 package util
 
 import (
-	"bufio"
-	"compress/gzip"
 	"encoding/hex"
 	"hash"
 	"io"
@@ -36,31 +34,14 @@ const (
 	DefaultFilePermissions      os.FileMode = 0644
 )
 
-type File struct {
-	io.ReadCloser
-	hash.Hash
-	Path        string
-	Mode        os.FileMode
-	Uid         int
-	Gid         int
-	expectedSum string
-}
-
-func (f File) Verify() error {
-	if f.Hash == nil {
-		return nil
-	}
-	sum := f.Sum(nil)
-	encodedSum := make([]byte, hex.EncodedLen(len(sum)))
-	hex.Encode(encodedSum, sum)
-
-	if string(encodedSum) != f.expectedSum {
-		return ErrHashMismatch{
-			Calculated: string(encodedSum),
-			Expected:   f.expectedSum,
-		}
-	}
-	return nil
+type FetchOp struct {
+	Hash         hash.Hash
+	Path         string
+	Mode         os.FileMode
+	Uid          int
+	Gid          int
+	Url          url.URL
+	FetchOptions resource.FetchOptions
 }
 
 // newHashedReader returns a new ReadCloser that also writes to the provided hash.
@@ -74,41 +55,33 @@ func newHashedReader(reader io.ReadCloser, hasher hash.Hash) io.ReadCloser {
 	}
 }
 
-// RenderFile returns a *File with a Reader that downloads, hashes, and decompresses the incoming data.
-// It returns nil if f had invalid options. Errors reading/verifying/decompressing the file will
-// present themselves when the Reader is actually read from.
-func RenderFile(l *log.Logger, c *resource.HttpClient, f types.File) *File {
-	var reader io.ReadCloser
+// PrepareFetch converts a given logger, http client, and types.File into a
+// FetchOp. This includes operations such as parsing the source URL, generating
+// a hasher, and performing user/group name lookups. If an error is encountered,
+// the issue will be logged and nil will be returned.
+func (u Util) PrepareFetch(l *log.Logger, c *resource.HttpClient, f types.File) *FetchOp {
 	var err error
-	var expectedSum string
+	var expectedSum []byte
 
 	// explicitly ignoring the error here because the config should already be
 	// validated by this point
-	u, _ := url.Parse(f.Contents.Source)
+	uri, _ := url.Parse(f.Contents.Source)
 
-	reader, err = resource.FetchAsReader(l, c, *u)
-	if err != nil {
-		l.Crit("Error fetching file %q: %v", f.Path, err)
-		return nil
-	}
-
-	fileHash, err := GetHasher(f.Contents.Verification)
+	hasher, err := GetHasher(f.Contents.Verification)
 	if err != nil {
 		l.Crit("Error verifying file %q: %v", f.Path, err)
 		return nil
 	}
 
-	if fileHash != nil {
-		reader = newHashedReader(reader, fileHash)
+	if hasher != nil {
 		// explicitly ignoring the error here because the config should already
 		// be validated by this point
-		_, expectedSum, _ = f.Contents.Verification.HashParts()
-	}
-
-	reader, err = decompressFileStream(l, f, reader)
-	if err != nil {
-		l.Crit("Error decompressing file %q: %v", f.Path, err)
-		return nil
+		_, expectedSumString, _ := f.Contents.Verification.HashParts()
+		expectedSum, err = hex.DecodeString(expectedSumString)
+		if err != nil {
+			l.Crit("Error parsing verification string %q: %v", expectedSumString, err)
+			return nil
+		}
 	}
 
 	if f.User.Name != "" {
@@ -140,53 +113,18 @@ func RenderFile(l *log.Logger, c *resource.HttpClient, f types.File) *File {
 		f.Group.ID = &tmp
 	}
 
-	return &File{
-		Path:        f.Path,
-		ReadCloser:  reader,
-		Hash:        fileHash,
-		Mode:        os.FileMode(f.Mode),
-		Uid:         *f.User.ID,
-		Gid:         *f.Group.ID,
-		expectedSum: expectedSum,
-	}
-}
-
-// gzipReader is a wrapper for gzip's reader that closes the stream it wraps as well
-// as itself when Close() is called.
-type gzipReader struct {
-	*gzip.Reader //actually a ReadCloser
-	source       io.Closer
-}
-
-func newGzipReader(reader io.ReadCloser) (io.ReadCloser, error) {
-	gzReader, err := gzip.NewReader(reader)
-	if err != nil {
-		return nil, err
-	}
-	return gzipReader{
-		Reader: gzReader,
-		source: reader,
-	}, nil
-}
-
-func (gz gzipReader) Close() error {
-	if err := gz.Reader.Close(); err != nil {
-		return err
-	}
-	if err := gz.source.Close(); err != nil {
-		return err
-	}
-	return nil
-}
-
-func decompressFileStream(l *log.Logger, f types.File, contents io.ReadCloser) (io.ReadCloser, error) {
-	switch f.Contents.Compression {
-	case "":
-		return contents, nil
-	case "gzip":
-		return newGzipReader(contents)
-	default:
-		return nil, types.ErrCompressionInvalid
+	return &FetchOp{
+		Path: f.Path,
+		Hash: hasher,
+		Mode: os.FileMode(f.Mode),
+		Uid:  *f.User.ID,
+		Gid:  *f.Group.ID,
+		Url:  *uri,
+		FetchOptions: resource.FetchOptions{
+			Hash:        hasher,
+			Compression: f.Contents.Compression,
+			ExpectedSum: expectedSum,
+		},
 	}
 }
 
@@ -203,9 +141,9 @@ func (u Util) WriteLink(s types.Link) error {
 	return os.Symlink(s.Target, path)
 }
 
-// WriteFile creates and writes the file described by f using the provided context.
-func (u Util) WriteFile(f *File) error {
-	defer f.Close()
+// PerformFetch performs a fetch operation generated by PrepareFetch, retrieving
+// the file and writing it to disk. Any encountered errors are returned.
+func (u Util) PerformFetch(f *FetchOp) error {
 	var err error
 
 	path := u.JoinPath(string(f.Path))
@@ -227,14 +165,9 @@ func (u Util) WriteFile(f *File) error {
 		}
 	}()
 
-	fileWriter := bufio.NewWriter(tmp)
-
-	if _, err = io.Copy(fileWriter, f); err != nil {
-		return err
-	}
-	fileWriter.Flush()
-
-	if err = f.Verify(); err != nil {
+	err = u.Fetcher.Fetch(f.Url, tmp, f.FetchOptions)
+	if err != nil {
+		u.Crit("Error fetching file %q: %v", f.Path, err)
 		return err
 	}
 

internal/exec/util/unit.go
@@ -15,13 +15,14 @@
 package util
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
+	"net/url"
 	"os"
 	"path/filepath"
 
 	"github.com/coreos/ignition/config/types"
+
+	"github.com/vincent-petithory/dataurl"
 )
 
 const (
@@ -29,28 +30,40 @@ const (
 	DefaultPresetPermissions os.FileMode = 0644
 )
 
-func FileFromSystemdUnit(unit types.Unit) *File {
-	return &File{
-		Path:       filepath.Join(SystemdUnitsPath(), string(unit.Name)),
-		ReadCloser: ioutil.NopCloser(bytes.NewReader([]byte(unit.Contents))),
-		Mode:       DefaultFilePermissions,
+func FileFromSystemdUnit(unit types.Unit) (*FetchOp, error) {
+	u, err := url.Parse(dataurl.EncodeBytes([]byte(unit.Contents)))
+	if err != nil {
+		return nil, err
 	}
+	return &FetchOp{
+		Path: filepath.Join(SystemdUnitsPath(), string(unit.Name)),
+		Url:  *u,
+		Mode: DefaultFilePermissions,
+	}, nil
 }
 
-func FileFromNetworkdUnit(unit types.Networkdunit) *File {
-	return &File{
-		Path:       filepath.Join(NetworkdUnitsPath(), string(unit.Name)),
-		ReadCloser: ioutil.NopCloser(bytes.NewReader([]byte(unit.Contents))),
-		Mode:       DefaultFilePermissions,
+func FileFromNetworkdUnit(unit types.Networkdunit) (*FetchOp, error) {
+	u, err := url.Parse(dataurl.EncodeBytes([]byte(unit.Contents)))
+	if err != nil {
+		return nil, err
 	}
+	return &FetchOp{
+		Path: filepath.Join(NetworkdUnitsPath(), string(unit.Name)),
+		Url:  *u,
+		Mode: DefaultFilePermissions,
+	}, nil
 }
 
-func FileFromUnitDropin(unit types.Unit, dropin types.Dropin) *File {
-	return &File{
-		Path:       filepath.Join(SystemdDropinsPath(string(unit.Name)), string(dropin.Name)),
-		ReadCloser: ioutil.NopCloser(bytes.NewReader([]byte(dropin.Contents))),
-		Mode:       DefaultFilePermissions,
+func FileFromUnitDropin(unit types.Unit, dropin types.Dropin) (*FetchOp, error) {
+	u, err := url.Parse(dataurl.EncodeBytes([]byte(dropin.Contents)))
+	if err != nil {
+		return nil, err
 	}
+	return &FetchOp{
+		Path: filepath.Join(SystemdDropinsPath(string(unit.Name)), string(dropin.Name)),
+		Url:  *u,
+		Mode: DefaultFilePermissions,
+	}, nil
 }
 
 func (u Util) MaskUnit(unit types.Unit) error {

internal/exec/util/util.go
@@ -18,11 +18,13 @@ import (
 	"path/filepath"
 
 	"github.com/coreos/ignition/internal/log"
+	"github.com/coreos/ignition/internal/resource"
 )
 
 // Util encapsulates logging and destdir indirection for the util methods.
 type Util struct {
 	DestDir string // directory prefix to use in applying fs paths.
+	Fetcher resource.Fetcher
 	*log.Logger
 }
 

internal/exec/util/verification.go
@@ -17,21 +17,11 @@ package util
 import (
 	"crypto/sha512"
 	"encoding/hex"
-	"fmt"
 	"hash"
 
 	"github.com/coreos/ignition/config/types"
+	"github.com/coreos/ignition/internal/resource"
 )
-
-type ErrHashMismatch struct {
-	Calculated string
-	Expected   string
-}
-
-func (e ErrHashMismatch) Error() string {
-	return fmt.Sprintf("hash verification failed (calculated %s but expected %s)",
-		e.Calculated, e.Expected)
-}
 
 func AssertValid(verify types.Verification, data []byte) error {
 	if hash := verify.Hash; hash != nil {
@@ -52,7 +42,7 @@ func AssertValid(verify types.Verification, data []byte) error {
 		encodedSum := make([]byte, hex.EncodedLen(len(sum)))
 		hex.Encode(encodedSum, sum)
 		if string(encodedSum) != hashSum {
-			return ErrHashMismatch{
+			return resource.ErrHashMismatch{
 				Calculated: string(encodedSum),
 				Expected:   hashSum,
 			}

commit cb4da35d49b42bece21fd39625b5b2607b47163c

stages/files: fix regression affecting file paths
This was broken by 8f6e802753404e. We need to create a new Util object
which has the desired filesystem-root for this specific file instead of
using the root.

internal/exec/stages/files/files.go
@@ -305,8 +305,14 @@ func (s stage) createEntries(fs types.Filesystem, files []filesystemEntry) error
 		mnt = *fs.Path
 	}
 
+	u := util.Util{
+		DestDir: mnt,
+		Fetcher: s.Util.Fetcher,
+		Logger:  s.Logger,
+	}
+
 	for _, e := range files {
-		if err := e.create(s.Logger, s.client, s.Util); err != nil {
+		if err := e.create(s.Logger, s.client, u); err != nil {
 			return err
 		}
 	}