0877760f18d95ed125d933c2d15bc3ac23248f82
Samuel El-Borai <samuel.el-borai@optiopay.com>
2017-08-18 12:16:42 +0200

    internal/oem: fix gcloud volume path

--- internal/oem/oem.go
+++ internal/oem/oem.go
@@ -199,9 +199,9 @@ func init() {
 						FileEmbedded1: types.FileEmbedded1{
 							Mode: 0444,
 							Contents: contentsFromString(`#!/bin/sh
-alias gcloud="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -t -i --net="host" -v $HOME/.config:/.config -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker google/cloud-sdk gcloud"
-alias gcutil="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -t -i --net="host" -v $HOME/.config:/.config google/cloud-sdk gcutil"
-alias gsutil="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -t -i --net="host" -v $HOME/.config:/.config google/cloud-sdk gsutil"
+alias gcloud="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -t -i --net="host" -v $HOME/.config:/root/.config -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker google/cloud-sdk gcloud"
+alias gcutil="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -t -i --net="host" -v $HOME/.config:/root/.config google/cloud-sdk gcutil"
+alias gsutil="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -t -i --net="host" -v $HOME/.config:/root/.config google/cloud-sdk gsutil"
 `),
 						},
 					},
