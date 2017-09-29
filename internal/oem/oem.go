222108bdf6a1fd0d5ddabca26e6a84fb78432ea6
Benjamin Gilbert <benjamin.gilbert@coreos.com>
2017-09-28 17:15:16 -0700

    oem: delete google/cloud-sdk containers on exit
    We were leaking a container for each gcloud/gsutil invocation.

--- internal/oem/oem.go
+++ internal/oem/oem.go
@@ -201,8 +201,8 @@ func init() {
 						FileEmbedded1: types.FileEmbedded1{
 							Mode: 0444,
 							Contents: contentsFromString(`#!/bin/sh
-alias gcloud="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -t -i --net="host" -v $HOME/.config:/root/.config -v /var/run/docker.sock:/var/run/docker.sock google/cloud-sdk gcloud"
-alias gsutil="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -t -i --net="host" -v $HOME/.config:/root/.config google/cloud-sdk gsutil"
+alias gcloud="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -ti --rm --net="host" -v $HOME/.config:/root/.config -v /var/run/docker.sock:/var/run/docker.sock google/cloud-sdk gcloud"
+alias gsutil="(docker images google/cloud-sdk || docker pull google/cloud-sdk) > /dev/null;docker run -ti --rm --net="host" -v $HOME/.config:/root/.config google/cloud-sdk gsutil"
 `),
 						},
 					},
