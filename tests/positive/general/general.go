cfc25afc97d779794708e7dedfd1011140679fd3
Stephen Lowrie <stephen.lowrie@gmail.com>
2017-11-17 15:16:26 -0800

    tests: add tests for data URI's in append/replace
    Adds tests covering using data URI's in append/replace. Also adds
    a TODO note to add s3 tests.

--- tests/positive/general/general.go
+++ tests/positive/general/general.go
@@ -20,6 +20,7 @@ import (
 )
 
 func init() {
+	// TODO: Add S3 tests
 	register.Register(register.PositiveTest, ReformatFilesystemAndWriteFile())
 	register.Register(register.PositiveTest, ReplaceConfigWithRemoteConfigHTTP())
 	register.Register(register.PositiveTest, AppendConfigWithRemoteConfigHTTP())
@@ -27,6 +28,8 @@ func init() {
 	register.Register(register.PositiveTest, AppendConfigWithRemoteConfigTFTP())
 	register.Register(register.PositiveTest, ReplaceConfigWithRemoteConfigOEM())
 	register.Register(register.PositiveTest, AppendConfigWithRemoteConfigOEM())
+	register.Register(register.PositiveTest, ReplaceConfigWithRemoteConfigData())
+	register.Register(register.PositiveTest, AppendConfigWithRemoteConfigData())
 	register.Register(register.PositiveTest, VersionOnlyConfig())
 	register.Register(register.PositiveTest, EmptyUserdata())
 }
@@ -339,6 +342,70 @@ func AppendConfigWithRemoteConfigOEM() types.Test {
 		{
 			Node: types.Node{
 				Name:      "bar2",
+				Directory: "foo",
+			},
+			Contents: "another example file\n",
+		},
+	})
+
+	return types.Test{
+		Name:   name,
+		In:     in,
+		Out:    out,
+		Config: config,
+	}
+}
+
+func ReplaceConfigWithRemoteConfigData() types.Test {
+	name := "Replacing the Config with a Remote Config from Data"
+	in := types.GetBaseDisk()
+	out := types.GetBaseDisk()
+	config := `{
+          "ignition": {
+            "version": "2.1.0",
+            "config": {
+              "replace": {
+				  "source": "data:,%7B%22ignition%22%3A%7B%22version%22%3A%20%222.1.0%22%7D%2C%22storage%22%3A%20%7B%22files%22%3A%20%5B%7B%22filesystem%22%3A%20%22root%22%2C%22path%22%3A%20%22%2Ffoo%2Fbar%22%2C%22contents%22%3A%7B%22source%22%3A%22data%3A%2Canother%2520example%2520file%250A%22%7D%7D%5D%7D%7D%0A"
+              }
+            }
+          }
+        }`
+	out[0].Partitions.AddFiles("ROOT", []types.File{
+		{
+			Node: types.Node{
+				Name:      "bar",
+				Directory: "foo",
+			},
+			Contents: "another example file\n",
+		},
+	})
+
+	return types.Test{
+		Name:   name,
+		In:     in,
+		Out:    out,
+		Config: config,
+	}
+}
+
+func AppendConfigWithRemoteConfigData() types.Test {
+	name := "Appending to the Config with a Remote Config from Data"
+	in := types.GetBaseDisk()
+	out := types.GetBaseDisk()
+	config := `{
+          "ignition": {
+            "version": "2.1.0",
+            "config": {
+              "append": [{
+				  "source": "data:,%7B%22ignition%22%3A%7B%22version%22%3A%20%222.1.0%22%7D%2C%22storage%22%3A%20%7B%22files%22%3A%20%5B%7B%22filesystem%22%3A%20%22root%22%2C%22path%22%3A%20%22%2Ffoo%2Fbar%22%2C%22contents%22%3A%7B%22source%22%3A%22data%3A%2Canother%2520example%2520file%250A%22%7D%7D%5D%7D%7D%0A"
+              }]
+            }
+          }
+        }`
+	out[0].Partitions.AddFiles("ROOT", []types.File{
+		{
+			Node: types.Node{
+				Name:      "bar",
 				Directory: "foo",
 			},
 			Contents: "another example file\n",
