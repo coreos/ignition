6977120e5e924eed018b21d925fa3f0999450db6
Derek Gonyeo <derek@gonyeo.com>
2018-03-08 16:03:11 -0800

    config/v2_2: fall back to v2_1 parsing and translation on error
    When an error is encountered (or a config that's too old), attempt to
    call v2_1 parsing and then translate it into a v2_2 config.

--- config/v2_2/config.go
+++ config/v2_2/config.go
@@ -15,18 +15,15 @@
 package v2_2
 
 import (
-	"bytes"
 	"errors"
-	"reflect"
 
+	"github.com/coreos/ignition/config/util"
+	"github.com/coreos/ignition/config/v2_1"
 	"github.com/coreos/ignition/config/v2_2/types"
-	"github.com/coreos/ignition/config/validate"
-	astjson "github.com/coreos/ignition/config/validate/astjson"
 	"github.com/coreos/ignition/config/validate/report"
 
 	json "github.com/ajeddeloh/go-json"
 	"github.com/coreos/go-semver/semver"
-	"go4.org/errorutil"
 )
 
 var (
@@ -52,102 +49,33 @@ func Parse(rawConfig []byte) (types.Config, report.Report, error) {
 		return types.Config{}, report.Report{}, ErrScript
 	}
 
-	version, err := Version(rawConfig)
-	if err != nil {
-		return types.Config{}, report.ReportFromError(err, report.EntryError), err
-	}
-	if (version != semver.Version{Major: 2, Minor: 2}) {
-		return types.Config{}, report.ReportFromError(ErrBadVersion, report.EntryError), ErrBadVersion
-	}
-
+	var err error
 	var config types.Config
 
-	// These errors are fatal and the config should not be further validated
-	if err = json.Unmarshal(rawConfig, &config); err == nil {
-		versionReport := config.Ignition.Validate()
-		if versionReport.IsFatal() {
-			return types.Config{}, versionReport, ErrInvalid
+	err = json.Unmarshal(rawConfig, &config)
+
+	version, semverErr := semver.NewVersion(config.Ignition.Version)
+
+	if err != nil || semverErr != nil || version.LessThan(types.MaxVersion) {
+		// We can fail unmarshaling if it's an older config. Attempt to parse
+		// it as such.
+		config, rpt, err := v2_1.Parse(rawConfig)
+		if err != nil {
+			return types.Config{}, rpt, err
 		}
+		return TranslateFromV2_1(config), rpt, err
 	}
 
-	// Handle json syntax and type errors first, since they are fatal but have offset info
-	if serr, ok := err.(*json.SyntaxError); ok {
-		line, col, highlight := errorutil.HighlightBytePosition(bytes.NewReader(rawConfig), serr.Offset)
-		return types.Config{},
-			report.Report{
-				Entries: []report.Entry{{
-					Kind:      report.EntryError,
-					Message:   serr.Error(),
-					Line:      line,
-					Column:    col,
-					Highlight: highlight,
-				}},
-			},
-			ErrInvalid
+	if *version != types.MaxVersion {
+		return types.Config{}, report.Report{}, ErrInvalid
 	}
 
-	if terr, ok := err.(*json.UnmarshalTypeError); ok {
-		line, col, highlight := errorutil.HighlightBytePosition(bytes.NewReader(rawConfig), terr.Offset)
-		return types.Config{},
-			report.Report{
-				Entries: []report.Entry{{
-					Kind:      report.EntryError,
-					Message:   terr.Error(),
-					Line:      line,
-					Column:    col,
-					Highlight: highlight,
-				}},
-			},
-			ErrInvalid
+	rpt := util.ValidateConfig(rawConfig, config)
+	if rpt.IsFatal() {
+		return types.Config{}, rpt, ErrInvalid
 	}
 
-	// Handle other fatal errors (i.e. invalid version)
-	if err != nil {
-		return types.Config{}, report.ReportFromError(err, report.EntryError), err
-	}
-
-	// Unmarshal again to a json.Node to get offset information for building a report
-	var ast json.Node
-	var r report.Report
-	configValue := reflect.ValueOf(config)
-	if err := json.Unmarshal(rawConfig, &ast); err != nil {
-		r.Add(report.Entry{
-			Kind:    report.EntryWarning,
-			Message: "Ignition could not unmarshal your config for reporting line numbers. This should never happen. Please file a bug.",
-		})
-		r.Merge(validate.ValidateWithoutSource(configValue))
-	} else {
-		r.Merge(validate.Validate(configValue, astjson.FromJsonRoot(ast), bytes.NewReader(rawConfig), true))
-	}
-
-	if r.IsFatal() {
-		return types.Config{}, r, ErrInvalid
-	}
-
-	return config, r, nil
-}
-
-func Version(rawConfig []byte) (semver.Version, error) {
-	var composite struct {
-		Version  *int `json:"ignitionVersion"`
-		Ignition struct {
-			Version *string `json:"version"`
-		} `json:"ignition"`
-	}
-
-	if json.Unmarshal(rawConfig, &composite) == nil {
-		if composite.Ignition.Version != nil {
-			v, err := types.Ignition{Version: *composite.Ignition.Version}.Semver()
-			if err != nil {
-				return semver.Version{}, err
-			}
-			return *v, nil
-		} else if composite.Version != nil {
-			return semver.Version{Major: int64(*composite.Version)}, nil
-		}
-	}
-
-	return semver.Version{}, ErrVersionIndeterminable
+	return config, rpt, nil
 }
 
 func isEmpty(userdata []byte) bool {
