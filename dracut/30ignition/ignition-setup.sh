478877e9971c16f33b801dabd031042861cdb724
Yolanda Robla <yroblamo@redhat.com>
2019-02-05 11:59:34 +0100

    Remove the check for OEM partition, not used

--- dracut/30ignition/ignition-setup.sh
+++ dracut/30ignition/ignition-setup.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
-# Set up /usr/lib/ignition, copying contents from /usr/share/oem.
 
 set -e
 
-case "$1" in
-normal)
-    src=/mnt/oem
-    mkdir -p "${src}"
-    mount /dev/disk/by-label/OEM "${src}"
-    # retry-umount may not be necessary, but be cautious
-    trap 'retry-umount "${src}"' EXIT
-    ;;
-pxe)
-    # OEM directory in the initramfs itself
-    src=/usr/share/oem
-    ;;
-*)
-    echo "Usage: $0 {normal|pxe}" >&2
-    exit 1
-esac
+# OEM directory in the initramfs itself
+src=/usr/share/oem
 
 dst=/usr/lib/ignition
 mkdir -p "${dst}"
