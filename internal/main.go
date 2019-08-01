ea00bcc8e1523a813521510e4e1c4287ce7ac026
Andrew Jeddeloh <ajeddelo@redhat.com>
2019-08-01 16:21:09 -0700

    main: log stage at startup

--- internal/main.go
+++ internal/main.go
@@ -71,6 +71,7 @@ func main() {
 	defer logger.Close()
 
 	logger.Info(version.String)
+	logger.Info("Stage: %v", flags.stage)
 
 	if flags.clearCache {
 		if err := os.Remove(flags.configCache); err != nil {
