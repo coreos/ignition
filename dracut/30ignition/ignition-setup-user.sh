3b0f845495c153c1608f69ee488ecef65aee7ae6
Benjamin Gilbert <bgilbert@redhat.com>
2019-09-09 21:33:21 -0400

    30ignition: on live images, read config.ign from custom initrd
    Allow the user to append a custom initrd containing /config.ign, and
    have ignition-setup-user copy it into place.  This means
    ignition-setup-user.service needs to Require the boot device only on
    diskful boots, so we need to generate that dependency in
    ignition-generator.

--- dracut/30ignition/ignition-setup-user.sh
+++ dracut/30ignition/ignition-setup-user.sh
@@ -14,11 +14,17 @@ copy_file_if_exists() {
 destination=/usr/lib/ignition
 mkdir -p $destination
 
-# We will support a user embedded config in the boot partition
-# under $bootmnt/ignition/config.ign. Note that we mount /boot
-# but we don't unmount boot because we are run in a systemd unit
-# with MountFlags=slave so it is unmounted for us.
-bootmnt=/mnt/boot_partition
-mkdir -p $bootmnt
-mount /dev/disk/by-label/boot $bootmnt
-copy_file_if_exists "${bootmnt}/ignition/config.ign" "${destination}/user.ign"
+if command -v is-live-image >/dev/null && is-live-image; then
+    # Live image. If the user has supplied a config.ign via an appended
+    # initrd, put it in the right place.
+    copy_file_if_exists "/config.ign" "${destination}/user.ign"
+else
+    # We will support a user embedded config in the boot partition
+    # under $bootmnt/ignition/config.ign. Note that we mount /boot
+    # but we don't unmount boot because we are run in a systemd unit
+    # with MountFlags=slave so it is unmounted for us.
+    bootmnt=/mnt/boot_partition
+    mkdir -p $bootmnt
+    mount /dev/disk/by-label/boot $bootmnt
+    copy_file_if_exists "${bootmnt}/ignition/config.ign" "${destination}/user.ign"
+fi
