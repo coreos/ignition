f5ae45b5da1c7e88541feb96a8d2055b6bdda8d6
Steve Milner <smilner@redhat.com>
2019-10-02 10:27:27 -0400

    providers/aliyun: Add aliyun
    See:
    - https://www.alibabacloud.com/help/doc-detail/49122.htm
    - https://github.com/coreos/ignition/issues/589
    
    Signed-off-by: Steve Milner <smilner@redhat.com>
    (cherry picked from commit 19d0d6caa7800379cca5a98291ebe90ced58a1a9)

---
+++ internal/providers/aliyun/aliyun.go
@@ -0,0 +1,47 @@
+// Copyright 2019 Red Hat
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// The aliyun provider fetches a remote configuration from the
+// aliyun user-data metadata service URL.
+
+package aliyun
+
+import (
+	"net/url"
+
+	"github.com/coreos/ignition/internal/config/types"
+	"github.com/coreos/ignition/internal/providers/util"
+	"github.com/coreos/ignition/internal/resource"
+
+	"github.com/coreos/ignition/config/validate/report"
+)
+
+var (
+	userdataUrl = url.URL{
+		Scheme: "http",
+		Host:   "100.100.100.200",
+		Path:   "latest/user-data",
+	}
+)
+
+func FetchConfig(f *resource.Fetcher) (types.Config, report.Report, error) {
+	data, err := f.FetchToBuffer(userdataUrl, resource.FetchOptions{
+		Headers: resource.ConfigHeaders,
+	})
+	if err != nil && err != resource.ErrNotFound {
+		return types.Config{}, report.Report{}, err
+	}
+
+	return util.ParseConfig(f.Logger, data)
+}
