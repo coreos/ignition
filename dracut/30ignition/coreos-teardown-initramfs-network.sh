70c7cd59961fe836402d7ba7562f81c5c6829d0e
Dusty Mabe <dusty@dustymabe.com>
2020-03-18 17:22:44 -0400

    coreos-teardown-initramfs-network: refactor into more functions
    This will make it easier for us to add a bit more code into this
    file.

--- dracut/30ignition/coreos-teardown-initramfs-network.sh
+++ dracut/30ignition/coreos-teardown-initramfs-network.sh
@@ -5,29 +5,45 @@
 set -euo pipefail
 
 down_interface() {
+    echo "info: taking down network device: $1"
     ip link set $1 down
     ip addr flush dev $1
     rm -f -- /tmp/net.$1.did-setup
 }
 
-# We want to take down the bonded interfaces first
-if [ -f "/sys/class/net/bonding_masters" ]; then
-    bonds="$(cat /sys/class/net/bonding_masters)"
-    for b in ${bonds[@]}; do
-        down_interface ${b}
-        echo -"${b}" > /sys/class/net/bonding_masters
-     done
-fi
+down_bonds() {
+    if [ -f "/sys/class/net/bonding_masters" ]; then
+        bonds="$(cat /sys/class/net/bonding_masters)"
+        for b in ${bonds[@]}; do
+            down_interface ${b}
+            echo -"${b}" > /sys/class/net/bonding_masters
+         done
+    fi
+}
 
-# Clean up the interfaces set up in the initramfs
 # This mimics the behaviour of dracut's ifdown() in net-lib.sh
-if ! [ -z "$(ls /sys/class/net)" ]; then
-    for f in /sys/class/net/*; do
-        interface=$(basename "$f")
-        # The `bonding_masters` entry is not a true interface and thus
-        # cannot be taken down.  If they existed, the bonded interfaces
-        # were taken down earlier in this script.
-        if [ "$interface" == "bonding_masters" ]; then continue; fi
-        down_interface $interface
-    done
-fi
+# Note that in the futre we would like to possibly use `nmcli` networking off`
+# for this. See the following two comments for details:
+# https://github.com/coreos/fedora-coreos-tracker/issues/394#issuecomment-599721763
+# https://github.com/coreos/fedora-coreos-tracker/issues/394#issuecomment-599746049
+down_interfaces() {
+    if ! [ -z "$(ls /sys/class/net)" ]; then
+        for f in /sys/class/net/*; do
+            interface=$(basename "$f")
+            # The `bonding_masters` entry is not a true interface and thus
+            # cannot be taken down.  If they existed, the bonded interfaces
+            # were taken down earlier in this script.
+            if [ "$interface" == "bonding_masters" ]; then continue; fi
+            down_interface $interface
+        done
+    fi
+}
+
+main() {
+    # We want to take down the bonded interfaces first
+    down_bonds
+    # Clean up the interfaces set up in the initramfs
+    down_interfaces
+}
+
+main
