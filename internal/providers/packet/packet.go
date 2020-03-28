coreos/ignition

commit 75d3e2da74ae42ab72b238a910aa674d6f51189e

Rework fetch/resource to automatically append required headers
Rather than having each platform provider pass down `resource.ConfigHeaders`,
automatically inject it.  Then each platform only needs to pass
additional headers, which in most cases is none.

Unexport `configHeaders` then.

This also avoids mutating a global variable (!).  Queue obligatory
crying for 3 seconds that this code isn't Rust.

Prep for further work around headers.

internal/providers/packet/packet.go
@@ -55,7 +55,7 @@ var (
 func FetchConfig(f *resource.Fetcher) (types.Config, report.Report, error) {
 	// Packet's metadata service returns "Not Acceptable" when queried
 	// with the default Accept header.
-	headers := resource.ConfigHeaders
+	headers := make(http.Header)
 	headers.Set("Accept", "*/*")
 	data, err := f.FetchToBuffer(userdataUrl, resource.FetchOptions{
 		Headers: headers,

internal/exec/engine.go
@@ -272,9 +272,7 @@ func (e *Engine) fetchReferencedConfig(cfgRef types.ConfigReference) (types.Conf
 	if err != nil {
 		return types.Config{}, err
 	}
-	rawCfg, err := e.Fetcher.FetchToBuffer(*u, resource.FetchOptions{
-		Headers: resource.ConfigHeaders,
-	})
+	rawCfg, err := e.Fetcher.FetchToBuffer(*u, resource.FetchOptions{})
 	if err != nil {
 		return types.Config{}, err
 	}

internal/providers/aliyun/aliyun.go
@@ -36,9 +36,7 @@ var (
 )
 
 func FetchConfig(f *resource.Fetcher) (types.Config, report.Report, error) {
-	data, err := f.FetchToBuffer(userdataUrl, resource.FetchOptions{
-		Headers: resource.ConfigHeaders,
-	})
+	data, err := f.FetchToBuffer(userdataUrl, resource.FetchOptions{})
 	if err != nil && err != resource.ErrNotFound {
 		return types.Config{}, report.Report{}, err
 	}

internal/providers/aws/aws.go
@@ -41,9 +41,7 @@ var (
 )
 
 func FetchConfig(f *resource.Fetcher) (types.Config, report.Report, error) {
-	data, err := f.FetchToBuffer(userdataUrl, resource.FetchOptions{
-		Headers: resource.ConfigHeaders,
-	})
+	data, err := f.FetchToBuffer(userdataUrl, resource.FetchOptions{})
 	if err != nil && err != resource.ErrNotFound {
 		return types.Config{}, report.Report{}, err
 	}

internal/providers/cloudstack/cloudstack.go
@@ -204,8 +204,6 @@ func fetchConfigFromMetadataService(f *resource.Fetcher) ([]byte, error) {
 		Path:   "/latest/user-data",
 	}
 
-	res, err := f.FetchToBuffer(metadataServiceUrl, resource.FetchOptions{
-		Headers: resource.ConfigHeaders,
-	})
+	res, err := f.FetchToBuffer(metadataServiceUrl, resource.FetchOptions{})
 	return res, err
 }

internal/providers/cmdline/cmdline.go
@@ -46,9 +46,7 @@ func FetchConfig(f *resource.Fetcher) (types.Config, report.Report, error) {
 		return types.Config{}, report.Report{}, providers.ErrNoProvider
 	}
 
-	data, err := f.FetchToBuffer(*url, resource.FetchOptions{
-		Headers: resource.ConfigHeaders,
-	})
+	data, err := f.FetchToBuffer(*url, resource.FetchOptions{})
 	if err != nil {
 		return types.Config{}, report.Report{}, err
 	}

internal/providers/digitalocean/digitalocean.go
@@ -36,9 +36,7 @@ var (
 )
 
 func FetchConfig(f *resource.Fetcher) (types.Config, report.Report, error) {
-	data, err := f.FetchToBuffer(userdataUrl, resource.FetchOptions{
-		Headers: resource.ConfigHeaders,
-	})
+	data, err := f.FetchToBuffer(userdataUrl, resource.FetchOptions{})
 	if err != nil {
 		return types.Config{}, report.Report{}, err
 	}

internal/providers/gcp/gcp.go
@@ -18,6 +18,7 @@
 package gcp
 
 import (
+	"net/http"
 	"net/url"
 
 	"github.com/coreos/ignition/v2/config/v3_1_experimental/types"
@@ -38,7 +39,7 @@ var (
 )
 
 func FetchConfig(f *resource.Fetcher) (types.Config, report.Report, error) {
-	headers := resource.ConfigHeaders
+	headers := make(http.Header)
 	headers.Set(metadataHeaderKey, metadataHeaderVal)
 	data, err := f.FetchToBuffer(userdataUrl, resource.FetchOptions{
 		Headers: headers,

commit 4c2dcd547b01b2f13ced8d35263ded5fd67fa645

providers/packet: override Accept header fetching instance metadata
Passing nil Headers is no longer sufficient to avoid the default Accept
header; we need to override it.  Fixes 406 Not Acceptable error.

Fixes: 75d3e2da74ae ("Rework fetch/resource to automatically append required headers")

internal/providers/packet/packet.go
@@ -71,8 +71,10 @@ func FetchConfig(f *resource.Fetcher) (types.Config, report.Report, error) {
 func PostStatus(stageName string, f resource.Fetcher, errMsg error) error {
 	f.Logger.Info("POST message to Packet Timeline")
 	// fetch JSON from https://metadata.packet.net/metadata
+	headers := make(http.Header)
+	headers.Set("Accept", "*/*")
 	data, err := f.FetchToBuffer(metadataUrl, resource.FetchOptions{
-		Headers: nil,
+		Headers: headers,
 	})
 	if err != nil {
 		return err