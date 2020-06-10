ca08d34f998f8a2bd7557dcb2387887e755de37c
Timothée Ravier <travier@redhat.com>
2020-06-10 19:31:01 +0200

    Check shasum from sources after download

--- download-ignition.sh
+++ download-ignition.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -xeuo pipefail
-rpmspec -P ignition.spec | grep 'Source0:' | tr -s ' ' | cut -d ' ' -f 2 | xargs wget 
+rpmspec -P ignition.spec | grep 'Source0:' | tr -s ' ' | cut -d ' ' -f 2 | xargs wget
+grep "ignition-[0-9a-z]*.tar.gz" sources | sha512sum -c -
