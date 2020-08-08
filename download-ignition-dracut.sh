0c8e5a6b3f0a90f3e4f31faa8927ca6c7cc5f832
Benjamin Gilbert <bgilbert@redhat.com>
2020-08-07 23:28:56 -0400

    Revert "Check shasum from sources after download"
    The check is circular; we can't check the hash against the sources file
    because we're downloading new sources that haven't yet been hashed into the
    sources file by `rhpkg upload`.  To download sources already listed in the
    sources file, use `rhpkg sources`.
    
    This reverts commit ca08d34f998f8a2bd7557dcb2387887e755de37c.

--- download-ignition-dracut.sh
+++ download-ignition-dracut.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -xeuo pipefail
-rpmspec -P ignition.spec | grep 'Source1:' | tr -s ' ' | cut -d ' ' -f 2 | xargs wget
-grep "ignition-dracut-[0-9a-z]*.tar.gz" sources | sha512sum -c -
+rpmspec -P ignition.spec | grep 'Source1:' | tr -s ' ' | cut -d ' ' -f 2 | xargs wget 
--- download-ignition.sh
+++ download-ignition.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -xeuo pipefail
-rpmspec -P ignition.spec | grep 'Source0:' | tr -s ' ' | cut -d ' ' -f 2 | xargs wget
-grep "ignition-[0-9a-z]*.tar.gz" sources | sha512sum -c -
+rpmspec -P ignition.spec | grep 'Source0:' | tr -s ' ' | cut -d ' ' -f 2 | xargs wget 
