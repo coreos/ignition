e2d3fa3a61b3f06d13e48eac905a942057d2277a
Sohan Kunkerkar <sohank2602@gmail.com>
2020-10-15 17:47:27 -0400

    providers/system: support .d directory for base config fragments
    Fixes https://github.com/coreos/ignition/issues/1101
    This change allows the two distrosi (i.e. FCOS and RHCOS) to carry
    different base.ign files with common elements. Also,this will support
    an additional .d directory for each platform ID.

--- internal/providers/system/system.go
+++ internal/providers/system/system.go
@@ -19,6 +19,7 @@ import (
 	"os"
 	"path/filepath"
 
+	latest "github.com/coreos/ignition/v2/config/v3_3_experimental"
 	"github.com/coreos/ignition/v2/config/v3_3_experimental/types"
 	"github.com/coreos/ignition/v2/internal/distro"
 	"github.com/coreos/ignition/v2/internal/log"
@@ -30,12 +31,25 @@ import (
 )
 
 const (
-	baseFilename = "base.ign"
 	userFilename = "user.ign"
 )
 
-func FetchBaseConfig(logger *log.Logger) (types.Config, report.Report, error) {
-	return fetchConfig(logger, baseFilename)
+// FetchBaseConfig fetches base config fragments from the `base.d` and platform config fragments from
+// the `base.platform.d/platform`(if available), and merge them in the right order.
+func FetchBaseConfig(logger *log.Logger, platformName string) (types.Config, report.Report, error) {
+	fullBaseConfig, fullReport, err := fetchBaseDirectoryConfig(logger, "base.d")
+	if err != nil {
+		return types.Config{}, fullReport, err
+	}
+
+	platformDir := filepath.Join("base.platform.d", platformName)
+	basePlatformDConfig, basePlatformDReport, err := fetchBaseDirectoryConfig(logger, platformDir)
+	if err != nil {
+		logger.Info("no config at %q: %v", platformDir, err)
+	}
+	fullBaseConfig = latest.Merge(fullBaseConfig, basePlatformDConfig)
+	fullReport.Merge(basePlatformDReport)
+	return fullBaseConfig, fullReport, nil
 }
 
 func FetchConfig(f *resource.Fetcher) (types.Config, report.Report, error) {
@@ -55,4 +69,32 @@ func fetchConfig(logger *log.Logger, filename string) (types.Config, report.Repo
 		return types.Config{}, report.Report{}, err
 	}
 	return util.ParseConfig(logger, rawConfig)
+}
+
+// fetchBaseDirectoryConfig is a helper function to merge all the base config fragments inside of a particular directory.
+func fetchBaseDirectoryConfig(logger *log.Logger, dir string) (types.Config, report.Report, error) {
+	var baseConfig types.Config
+	var report report.Report
+	path := filepath.Join(distro.SystemConfigDir(), dir)
+	configs, err := ioutil.ReadDir(path)
+	if os.IsNotExist(err) {
+		logger.Info("no config dir at %q", path)
+		return types.Config{}, report, nil
+	} else if err != nil {
+		logger.Err("couldn't read config dir %q: %v", path, err)
+		return types.Config{}, report, err
+	}
+	if len(configs) == 0 {
+		logger.Info("no configs at %q", path)
+		return types.Config{}, report, nil
+	}
+	for _, config := range configs {
+		intermediateConfig, intermediateReport, err := fetchConfig(logger, filepath.Join(dir, config.Name()))
+		if err != nil {
+			return types.Config{}, intermediateReport, err
+		}
+		baseConfig = latest.Merge(baseConfig, intermediateConfig)
+		report.Merge(intermediateReport)
+	}
+	return baseConfig, report, nil
 }
