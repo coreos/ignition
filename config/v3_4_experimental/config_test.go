coreos/ignition

commit 500c68426fa1e1581b521138956012ebf765892b

config/*: add export functions for parsing any config version < N
Closes https://github.com/coreos/ignition/issues/1190
This change helps external callers to import the package for the most recent
version that they support and returns the desired config by calling a
function that accepts any config version up to and including that version.

config/v3_2/config.go
@@ -18,6 +18,8 @@ import (
 	"github.com/coreos/ignition/v2/config/merge"
 	"github.com/coreos/ignition/v2/config/shared/errors"
 	"github.com/coreos/ignition/v2/config/util"
+	prev "github.com/coreos/ignition/v2/config/v3_1"
+	"github.com/coreos/ignition/v2/config/v3_2/translate"
 	"github.com/coreos/ignition/v2/config/v3_2/types"
 	"github.com/coreos/ignition/v2/config/validate"
 
@@ -54,4 +56,23 @@ func Parse(rawConfig []byte) (types.Config, report.Report, error) {
 	}
 
 	return config, rpt, nil
+}
+
+// ParseCompatibleVersion parses the raw config of version 3.2.0 or lesser
+// into a 3.2 types.Config struct and generates a report of any errors, warnings,
+// info, and deprecations it encountered
+func ParseCompatibleVersion(raw []byte) (types.Config, report.Report, error) {
+	version, rpt, err := util.GetConfigVersion(raw)
+	if err != nil {
+		return types.Config{}, rpt, err
+	}
+
+	if version == types.MaxVersion {
+		return Parse(raw)
+	}
+	prevCfg, r, err := prev.ParseCompatibleVersion(raw)
+	if err != nil {
+		return types.Config{}, report.Report{}, err
+	}
+	return translate.Translate(prevCfg), r, nil
 }

config/v3_0/config_test.go
@@ -101,8 +101,40 @@ func TestParse(t *testing.T) {
 		},
 	}
 
+	testsCompt := []struct {
+		in  in
+		out out
+	}{
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.0.0"}}`)},
+			out: out{config: types.Config{Ignition: types.Ignition{Version: types.MaxVersion.String()}}},
+		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.1.0"}}`)},
+			out: out{err: errors.ErrUnknownVersion},
+		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.2.0"}}`)},
+			out: out{err: errors.ErrUnknownVersion},
+		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.3.0-experimental"}}`)},
+			out: out{err: errors.ErrUnknownVersion},
+		},
+		{
+			in:  in{config: []byte{}},
+			out: out{err: errors.ErrEmpty},
+		},
+	}
 	for i, test := range tests {
 		config, report, err := Parse(test.in.config)
+		if test.out.err != err {
+			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
+		}
+		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
+	}
+	for i, test := range testsCompt {
+		config, report, err := ParseCompatibleVersion(test.in.config)
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
 		}

config/v3_1/config_test.go
@@ -121,8 +121,37 @@ func TestParse(t *testing.T) {
 		},
 	}
 
+	testsCompt := []struct {
+		in  in
+		out out
+	}{
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.2.0"}}`)},
+			out: out{err: errors.ErrUnknownVersion},
+		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.0.0"}}`)},
+			out: out{config: types.Config{Ignition: types.Ignition{Version: types.MaxVersion.String()}}},
+		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.1.0"}}`)},
+			out: out{config: types.Config{Ignition: types.Ignition{Version: types.MaxVersion.String()}}},
+		},
+		{
+			in:  in{config: []byte{}},
+			out: out{err: errors.ErrEmpty},
+		},
+	}
+
 	for i, test := range tests {
 		config, report, err := Parse(test.in.config)
+		if test.out.err != err {
+			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
+		}
+		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
+	}
+	for i, test := range testsCompt {
+		config, report, err := ParseCompatibleVersion(test.in.config)
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
 		}

config/v3_2/config_test.go
@@ -129,8 +129,41 @@ func TestParse(t *testing.T) {
 		},
 	}
 
+	testsCompt := []struct {
+		in  in
+		out out
+	}{
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.2.0"}}`)},
+			out: out{config: types.Config{Ignition: types.Ignition{Version: types.MaxVersion.String()}}},
+		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.0.0"}}`)},
+			out: out{config: types.Config{Ignition: types.Ignition{Version: types.MaxVersion.String()}}},
+		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.1.0"}}`)},
+			out: out{config: types.Config{Ignition: types.Ignition{Version: types.MaxVersion.String()}}},
+		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.3.0-experimental"}}`)},
+			out: out{err: errors.ErrUnknownVersion},
+		},
+		{
+			in:  in{config: []byte{}},
+			out: out{err: errors.ErrEmpty},
+		},
+	}
+
 	for i, test := range tests {
 		config, report, err := Parse(test.in.config)
+		if test.out.err != err {
+			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
+		}
+		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
+	}
+	for i, test := range testsCompt {
+		config, report, err := ParseCompatibleVersion(test.in.config)
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
 		}

config/config.go
@@ -15,18 +15,7 @@
 package config
 
 import (
-	"github.com/coreos/ignition/v2/config/shared/errors"
-	"github.com/coreos/ignition/v2/config/util"
-	"github.com/coreos/ignition/v2/config/v3_0"
-	types_3_0 "github.com/coreos/ignition/v2/config/v3_0/types"
-	"github.com/coreos/ignition/v2/config/v3_1"
-	trans_3_1 "github.com/coreos/ignition/v2/config/v3_1/translate"
-	types_3_1 "github.com/coreos/ignition/v2/config/v3_1/types"
-	"github.com/coreos/ignition/v2/config/v3_2"
-	trans_3_2 "github.com/coreos/ignition/v2/config/v3_2/translate"
-	types_3_2 "github.com/coreos/ignition/v2/config/v3_2/types"
-	"github.com/coreos/ignition/v2/config/v3_3_experimental"
-	trans_exp "github.com/coreos/ignition/v2/config/v3_3_experimental/translate"
+	exp "github.com/coreos/ignition/v2/config/v3_3_experimental"
 	types_exp "github.com/coreos/ignition/v2/config/v3_3_experimental/types"
 
 	"github.com/coreos/vcontext/report"
@@ -35,41 +24,5 @@ import (
 // Parse parses a config of any supported version and returns the equivalent config at the latest
 // supported version.
 func Parse(raw []byte) (types_exp.Config, report.Report, error) {
-	version, r, err := util.GetConfigVersion(raw)
-	if err != nil {
-		return types_exp.Config{}, r, err
-	}
-	switch version {
-	case types_exp.MaxVersion:
-		return v3_3_experimental.Parse(raw)
-	case types_3_2.MaxVersion:
-		return exp_from_3_2(v3_2.Parse(raw))
-	case types_3_1.MaxVersion:
-		return exp_from_3_2(v3_2_from_3_1(v3_1.Parse(raw)))
-	case types_3_0.MaxVersion:
-		return exp_from_3_2(v3_2_from_3_1(v3_1_from_3_0(v3_0.Parse(raw))))
-	default:
-		return types_exp.Config{}, report.Report{}, errors.ErrUnknownVersion
-	}
-}
-
-func exp_from_3_2(cfg types_3_2.Config, r report.Report, err error) (types_exp.Config, report.Report, error) {
-	if err != nil {
-		return types_exp.Config{}, r, err
-	}
-	return trans_exp.Translate(cfg), r, nil
-}
-
-func v3_2_from_3_1(cfg types_3_1.Config, r report.Report, err error) (types_3_2.Config, report.Report, error) {
-	if err != nil {
-		return types_3_2.Config{}, r, err
-	}
-	return trans_3_2.Translate(cfg), r, nil
-}
-
-func v3_1_from_3_0(cfg types_3_0.Config, r report.Report, err error) (types_3_1.Config, report.Report, error) {
-	if err != nil {
-		return types_3_1.Config{}, r, err
-	}
-	return trans_3_1.Translate(cfg), r, nil
+	return exp.ParseCompatibleVersion(raw)
 }

config/v3_0/config.go
@@ -55,3 +55,19 @@ func Parse(rawConfig []byte) (types.Config, report.Report, error) {
 
 	return config, rpt, nil
 }
+
+// ParseCompatibleVersion parses the raw config of version 3.0.0 into
+// a 3.0 types.Config struct and generates a report of any errors, warnings,
+// info, and deprecations it encountered
+func ParseCompatibleVersion(raw []byte) (types.Config, report.Report, error) {
+	version, rpt, err := util.GetConfigVersion(raw)
+	if err != nil {
+		return types.Config{}, rpt, err
+	}
+
+	if version == types.MaxVersion {
+		return Parse(raw)
+	}
+
+	return types.Config{}, report.Report{}, errors.ErrUnknownVersion
+}

config/v3_1/config.go
@@ -18,6 +18,8 @@ import (
 	"github.com/coreos/ignition/v2/config/merge"
 	"github.com/coreos/ignition/v2/config/shared/errors"
 	"github.com/coreos/ignition/v2/config/util"
+	prev "github.com/coreos/ignition/v2/config/v3_0"
+	"github.com/coreos/ignition/v2/config/v3_1/translate"
 	"github.com/coreos/ignition/v2/config/v3_1/types"
 	"github.com/coreos/ignition/v2/config/validate"
 
@@ -54,4 +56,23 @@ func Parse(rawConfig []byte) (types.Config, report.Report, error) {
 	}
 
 	return config, rpt, nil
+}
+
+// ParseCompatibleVersion parses the raw config of version 3.1.0 or lesser
+// into a 3.1 types.Config struct and generates a report of any errors, warnings,
+// info, and deprecations it encountered
+func ParseCompatibleVersion(raw []byte) (types.Config, report.Report, error) {
+	version, rpt, err := util.GetConfigVersion(raw)
+	if err != nil {
+		return types.Config{}, rpt, err
+	}
+
+	if version == types.MaxVersion {
+		return Parse(raw)
+	}
+	prevCfg, r, err := prev.ParseCompatibleVersion(raw)
+	if err != nil {
+		return types.Config{}, r, err
+	}
+	return translate.Translate(prevCfg), r, nil
 }

config/v3_3_experimental/config.go
@@ -18,6 +18,8 @@ import (
 	"github.com/coreos/ignition/v2/config/merge"
 	"github.com/coreos/ignition/v2/config/shared/errors"
 	"github.com/coreos/ignition/v2/config/util"
+	prev "github.com/coreos/ignition/v2/config/v3_2"
+	"github.com/coreos/ignition/v2/config/v3_3_experimental/translate"
 	"github.com/coreos/ignition/v2/config/v3_3_experimental/types"
 	"github.com/coreos/ignition/v2/config/validate"
 
@@ -54,4 +56,23 @@ func Parse(rawConfig []byte) (types.Config, report.Report, error) {
 	}
 
 	return config, rpt, nil
+}
+
+// ParseCompatibleVersion parses the raw config of version 3.3.0-experimental or
+// lesser into a 3.3-exp types.Config struct and generates a report of any errors,
+// warnings, info, and deprecations it encountered
+func ParseCompatibleVersion(raw []byte) (types.Config, report.Report, error) {
+	version, rpt, err := util.GetConfigVersion(raw)
+	if err != nil {
+		return types.Config{}, rpt, err
+	}
+
+	if version == types.MaxVersion {
+		return Parse(raw)
+	}
+	prevCfg, r, err := prev.ParseCompatibleVersion(raw)
+	if err != nil {
+		return types.Config{}, report.Report{}, err
+	}
+	return translate.Translate(prevCfg), r, nil
 }

commit 516e2c218cade7af5b374c8ac02276b8d394056b

config/*: return report from previous parser when chaining
When we chain into the previous parser and it fails, return its report
rather than an empty one.  3.1 was doing this correctly and 3.0 doesn't
have this code, but newer specs were affected.

Add tests checking that both parse functions return a report on invalid
configs.

Fixes: 500c68426fa1 ("config/*: add export functions for parsing any config version < N")

config/v3_2/config.go
@@ -72,7 +72,7 @@ func ParseCompatibleVersion(raw []byte) (types.Config, report.Report, error) {
 	}
 	prevCfg, r, err := prev.ParseCompatibleVersion(raw)
 	if err != nil {
-		return types.Config{}, report.Report{}, err
+		return types.Config{}, r, err
 	}
 	return translate.Translate(prevCfg), r, nil
 }

config/v3_3/config.go
@@ -72,7 +72,7 @@ func ParseCompatibleVersion(raw []byte) (types.Config, report.Report, error) {
 	}
 	prevCfg, r, err := prev.ParseCompatibleVersion(raw)
 	if err != nil {
-		return types.Config{}, report.Report{}, err
+		return types.Config{}, r, err
 	}
 	return translate.Translate(prevCfg), r, nil
 }

config/v3_4_experimental/config.go
@@ -72,7 +72,7 @@ func ParseCompatibleVersion(raw []byte) (types.Config, report.Report, error) {
 	}
 	prevCfg, r, err := prev.ParseCompatibleVersion(raw)
 	if err != nil {
-		return types.Config{}, report.Report{}, err
+		return types.Config{}, r, err
 	}
 	return translate.Translate(prevCfg), r, nil
 }

config/v3_0/config_test.go
@@ -99,6 +99,10 @@ func TestParse(t *testing.T) {
 			in:  in{config: []byte{}},
 			out: out{err: errors.ErrEmpty},
 		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.0.0"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
 	}
 
 	testsCompt := []struct {
@@ -129,11 +133,18 @@ func TestParse(t *testing.T) {
 			in:  in{config: []byte{}},
 			out: out{err: errors.ErrEmpty},
 		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.0.0"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
 	}
 	for i, test := range tests {
 		config, report, err := Parse(test.in.config)
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
+		}
+		if test.out.err == errors.ErrInvalid && len(report.Entries) == 0 {
+			t.Errorf("#%d: expected report, got none", i)
 		}
 		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
 	}
@@ -141,6 +152,9 @@ func TestParse(t *testing.T) {
 		config, report, err := ParseCompatibleVersion(test.in.config)
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
+		}
+		if test.out.err == errors.ErrInvalid && len(report.Entries) == 0 {
+			t.Errorf("#%d: expected report, got none", i)
 		}
 		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
 	}

config/v3_1/config_test.go
@@ -119,6 +119,10 @@ func TestParse(t *testing.T) {
 			in:  in{config: []byte{}},
 			out: out{err: errors.ErrEmpty},
 		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.1.0"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
 	}
 
 	testsCompt := []struct {
@@ -149,6 +153,14 @@ func TestParse(t *testing.T) {
 			in:  in{config: []byte{}},
 			out: out{err: errors.ErrEmpty},
 		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.0.0"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.1.0"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
 	}
 
 	for i, test := range tests {
@@ -156,12 +168,18 @@ func TestParse(t *testing.T) {
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
 		}
+		if test.out.err == errors.ErrInvalid && len(report.Entries) == 0 {
+			t.Errorf("#%d: expected report, got none", i)
+		}
 		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
 	}
 	for i, test := range testsCompt {
 		config, report, err := ParseCompatibleVersion(test.in.config)
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
+		}
+		if test.out.err == errors.ErrInvalid && len(report.Entries) == 0 {
+			t.Errorf("#%d: expected report, got none", i)
 		}
 		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
 	}

config/v3_2/config_test.go
@@ -127,6 +127,10 @@ func TestParse(t *testing.T) {
 			in:  in{config: []byte{}},
 			out: out{err: errors.ErrEmpty},
 		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.2.0"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
 	}
 
 	testsCompt := []struct {
@@ -157,6 +161,14 @@ func TestParse(t *testing.T) {
 			in:  in{config: []byte{}},
 			out: out{err: errors.ErrEmpty},
 		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.0.0"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.2.0"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
 	}
 
 	for i, test := range tests {
@@ -164,12 +176,18 @@ func TestParse(t *testing.T) {
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
 		}
+		if test.out.err == errors.ErrInvalid && len(report.Entries) == 0 {
+			t.Errorf("#%d: expected report, got none", i)
+		}
 		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
 	}
 	for i, test := range testsCompt {
 		config, report, err := ParseCompatibleVersion(test.in.config)
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
+		}
+		if test.out.err == errors.ErrInvalid && len(report.Entries) == 0 {
+			t.Errorf("#%d: expected report, got none", i)
 		}
 		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
 	}

config/v3_3/config_test.go
@@ -135,6 +135,10 @@ func TestParse(t *testing.T) {
 			in:  in{config: []byte{}},
 			out: out{err: errors.ErrEmpty},
 		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.3.0"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
 	}
 
 	testsCompt := []struct {
@@ -165,6 +169,14 @@ func TestParse(t *testing.T) {
 			in:  in{config: []byte{}},
 			out: out{err: errors.ErrEmpty},
 		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.0.0"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.3.0"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
 	}
 
 	for i, test := range tests {
@@ -172,12 +184,18 @@ func TestParse(t *testing.T) {
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
 		}
+		if test.out.err == errors.ErrInvalid && len(report.Entries) == 0 {
+			t.Errorf("#%d: expected report, got none", i)
+		}
 		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
 	}
 	for i, test := range testsCompt {
 		config, report, err := ParseCompatibleVersion(test.in.config)
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
+		}
+		if test.out.err == errors.ErrInvalid && len(report.Entries) == 0 {
+			t.Errorf("#%d: expected report, got none", i)
 		}
 		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
 	}

config/v3_4_experimental/config_test.go
@@ -139,6 +139,10 @@ func TestParse(t *testing.T) {
 			in:  in{config: []byte{}},
 			out: out{err: errors.ErrEmpty},
 		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.4.0-experimental"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
 	}
 
 	testsCompt := []struct {
@@ -173,6 +177,14 @@ func TestParse(t *testing.T) {
 			in:  in{config: []byte{}},
 			out: out{err: errors.ErrEmpty},
 		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.0.0"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
+		{
+			in:  in{config: []byte(`{"ignition": {"version": "3.4.0-experimental"}, "storage": {"filesystems": [{"format": "ext4", "label": "zzzzzzzzzzzzzzzzzzzzzzzzzzz"}]}}`)},
+			out: out{err: errors.ErrInvalid},
+		},
 	}
 
 	for i, test := range tests {
@@ -180,12 +192,18 @@ func TestParse(t *testing.T) {
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
 		}
+		if test.out.err == errors.ErrInvalid && len(report.Entries) == 0 {
+			t.Errorf("#%d: expected report, got none", i)
+		}
 		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
 	}
 	for i, test := range testsCompt {
 		config, report, err := ParseCompatibleVersion(test.in.config)
 		if test.out.err != err {
 			t.Errorf("#%d: bad error: want %v, got %v, report: %+v", i, test.out.err, err, report)
+		}
+		if test.out.err == errors.ErrInvalid && len(report.Entries) == 0 {
+			t.Errorf("#%d: expected report, got none", i)
 		}
 		assert.Equal(t, test.out.config, config, "#%d: bad config, report: %+v", i, report)
 	}