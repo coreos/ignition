coreos/ignition

commit 65e9c1611128a3b4822a7ab10ebc565be0efc669

stages/disks: use State to persist keyfiles for files stage

internal/exec/stages/disks/luks.go
@@ -25,7 +25,6 @@ import (
 	"os"
 	"os/exec"
 	"path"
-	"path/filepath"
 	"strings"
 
 	"github.com/coreos/ignition/v2/config/util"
@@ -33,6 +32,8 @@ import (
 	"github.com/coreos/ignition/v2/internal/distro"
 	execUtil "github.com/coreos/ignition/v2/internal/exec/util"
 	"github.com/coreos/ignition/v2/internal/resource"
+
+	"github.com/vincent-petithory/dataurl"
 )
 
 var (
@@ -104,6 +105,8 @@ func (s *stage) createLuks(config types.Config) error {
 		return err
 	}
 
+	s.State.LuksPersistKeyFiles = make(map[string]string)
+
 	for _, luks := range config.Storage.Luks {
 		// TODO: allow Ignition generated KeyFiles for
 		// non-clevis devices that can be persisted.
@@ -111,15 +114,17 @@ func (s *stage) createLuks(config types.Config) error {
 		// track whether Ignition creates the KeyFile
 		// so that it can be removed
 		var ignitionCreatedKeyFile bool
-		// create keyfile inside of tmpfs, it will be copied to the
-		// sysroot by the files stage
-		if err := os.MkdirAll(distro.LuksInitramfsKeyFilePath(), 0700); err != nil {
-			return fmt.Errorf("creating directory for keyfile: %v", err)
+		// create keyfile, remove on the way out
+		keyFile, err := ioutil.TempFile("", "ignition-luks-")
+		if err != nil {
+			return fmt.Errorf("creating keyfile: %w", err)
 		}
-		keyFilePath := filepath.Join(distro.LuksInitramfsKeyFilePath(), luks.Name)
+		keyFilePath := keyFile.Name()
+		keyFile.Close()
+		defer os.Remove(keyFilePath)
 		devAlias := execUtil.DeviceAlias(*luks.Device)
 		if util.NilOrEmpty(luks.KeyFile.Source) {
-			// create a keyfile
+			// generate keyfile contents
 			key, err := randHex(4096)
 			if err != nil {
 				return fmt.Errorf("generating keyfile: %v", err)
@@ -316,17 +321,21 @@ func (s *stage) createLuks(config types.Config) error {
 			}
 		}
 
-		// assume the user does not want a key file & remove it for clevis based devices
 		if ignitionCreatedKeyFile && luks.Clevis.IsPresent() {
+			// assume the user does not want the generated key & remove it
 			if _, err := s.Logger.LogCmd(
 				exec.Command(distro.CryptsetupCmd(), "luksRemoveKey", devAlias, keyFilePath),
 				"removing key file for %v", luks.Name,
 			); err != nil {
 				return fmt.Errorf("removing key file from luks device: %v", err)
 			}
-			if err := os.Remove(keyFilePath); err != nil {
-				return fmt.Errorf("removing key file: %v", err)
+		} else {
+			// store the key to be persisted into the real root
+			key, err := ioutil.ReadFile(keyFilePath)
+			if err != nil {
+				return fmt.Errorf("failed to read keyfile %q: %w", keyFilePath, err)
 			}
+			s.State.LuksPersistKeyFiles[luks.Name] = dataurl.EncodeBytes(key)
 		}
 	}
 

internal/distro/distro.go
@@ -74,8 +74,7 @@ var (
 	// ".ssh/authorized_keys" ("false").
 	writeAuthorizedKeysFragment = "true"
 
-	luksInitramfsKeyFilePath = "/run/ignition/luks-keyfiles/"
-	luksRealRootKeyFilePath  = "/etc/luks/"
+	luksRealRootKeyFilePath = "/etc/luks/"
 )
 
 func DiskByIDDir() string       { return diskByIDDir }
@@ -113,8 +112,7 @@ func CryptsetupCmd() string { return cryptsetupCmd }
 
 func KargsCmd() string { return kargsCmd }
 
-func LuksInitramfsKeyFilePath() string { return luksInitramfsKeyFilePath }
-func LuksRealRootKeyFilePath() string  { return luksRealRootKeyFilePath }
+func LuksRealRootKeyFilePath() string { return luksRealRootKeyFilePath }
 
 func SelinuxRelabel() bool  { return bakedStringToBool(selinuxRelabel) && !BlackboxTesting() }
 func BlackboxTesting() bool { return bakedStringToBool(blackboxTesting) }

internal/exec/stages/files/filesystemEntries.go
@@ -16,7 +16,6 @@ package files
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -70,12 +69,11 @@ func (s *stage) createCrypttabEntries(config types.Config) error {
 		if !luks.Clevis.IsPresent() {
 			keyfile = filepath.Join(distro.LuksRealRootKeyFilePath(), luks.Name)
 
-			// Copy keyfile from /run to sysroot
-			contents, err := ioutil.ReadFile(filepath.Join(distro.LuksInitramfsKeyFilePath(), luks.Name))
-			if err != nil {
-				return fmt.Errorf("reading keyfile for %s: %v", luks.Name, err)
+			// Write keyfile into sysroot
+			contentsUri, ok := s.State.LuksPersistKeyFiles[luks.Name]
+			if !ok {
+				return fmt.Errorf("missing persisted keyfile for %s", luks.Name)
 			}
-			contentsUri := dataurl.EncodeBytes(contents)
 			keyfilePath, err := s.JoinPath(keyfile)
 			if err != nil {
 				return fmt.Errorf("building keyfile path: %v", err)
@@ -127,11 +125,9 @@ func (s *stage) createCrypttabEntries(config types.Config) error {
 	if err := s.createEntries(extrafiles); err != nil {
 		return fmt.Errorf("adding luks related files: %v", err)
 	}
-	// delete the entire keyfiles folder in /run/ so that the keyfiles are stored on
+	// delete the persisted keyfiles from state so that the keyfiles are stored on
 	// only the root device which can be encrypted
-	if err := os.RemoveAll(distro.LuksInitramfsKeyFilePath()); err != nil {
-		return fmt.Errorf("removing initramfs keyfiles: %v", err)
-	}
+	s.State.LuksPersistKeyFiles = nil
 	return nil
 }
 

internal/state/state.go
@@ -23,6 +23,11 @@ import (
 )
 
 type State struct {
+	// Key files generated during LUKS setup in disks stage, which need
+	// to be written out during files stage.  files stage removes them
+	// from state afterward to avoid leaking the keys into the running
+	// system.
+	LuksPersistKeyFiles map[string]string `json:"luksPersistKeyFiles"`
 }
 
 func Load(path string) (State, error) {

commit aed47c18aee593d155d45c0fe9ba29a9e3123cf6

disks: fix reuse of statically keyed LUKS volume
We need to persist a volume's keyfile to the real root even if we take
the early `continue` when reusing the volume.  Rather than copying code,
enable persistence up front and then disable it afterward if we decide
not to persist the key.

Fixes error:

    CRITICAL : Ignition failed: creating crypttab entries: missing persisted keyfile for [...]

Fixes: https://github.com/coreos/ignition/issues/1305
Fixes: 65e9c1611128 ("stages/disks: use State to persist keyfiles for files stage")

internal/exec/stages/disks/luks.go
@@ -156,6 +156,13 @@ func (s *stage) createLuks(config types.Config) error {
 				}
 			}
 		}
+		// store the key to be persisted into the real root
+		// do this here so device reuse works correctly
+		key, err := ioutil.ReadFile(keyFilePath)
+		if err != nil {
+			return fmt.Errorf("failed to read keyfile %q: %w", keyFilePath, err)
+		}
+		s.State.LuksPersistKeyFiles[luks.Name] = dataurl.EncodeBytes(key)
 
 		if !util.IsTrue(luks.WipeVolume) {
 			// If the volume isn't forcefully being created, then we need
@@ -329,13 +336,7 @@ func (s *stage) createLuks(config types.Config) error {
 			); err != nil {
 				return fmt.Errorf("removing key file from luks device: %v", err)
 			}
-		} else {
-			// store the key to be persisted into the real root
-			key, err := ioutil.ReadFile(keyFilePath)
-			if err != nil {
-				return fmt.Errorf("failed to read keyfile %q: %w", keyFilePath, err)
-			}
-			s.State.LuksPersistKeyFiles[luks.Name] = dataurl.EncodeBytes(key)
+			delete(s.State.LuksPersistKeyFiles, luks.Name)
 		}
 	}
 