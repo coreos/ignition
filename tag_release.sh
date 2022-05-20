e649cbd9c83573391c9255ab8d008462f051162c
Benjamin Gilbert <bgilbert@redhat.com>
2022-05-20 03:12:14 -0400

    NEWS: convert to Markdown

--- tag_release.sh
+++ tag_release.sh
@@ -19,12 +19,6 @@ COMMIT=$2
 
 source ./build
 
-# TODO(vc): generate NEWS as part of the release process.
-# @marineam suggested using git notes to associate NEWS-destined payloads
-# with objects, we just need to define a syntax and employ them.
-# I would like to be able to write the NEWS annotation as part of the commit message,
-# while still having it go into a note.
-
 git tag --sign --message "Ignition ${VER}" "${VER}" "${COMMIT}"
 
 git verify-tag --verbose "${VER}"
