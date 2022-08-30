b099378b7b9320a4c28c5ab959590bcad4b4793b
Steven Presti <prestist@gmail.com>
2022-08-30 18:01:04 -0400

    storage.go: refactor validate()
    Separate validate() into more granular subfunctions which describe the unit
    of work they are performing to improve readability and testability.

--- config/v3_0/types/storage.go
+++ config/v3_0/types/storage.go
@@ -34,6 +34,14 @@ func (s Storage) MergedKeys() map[string]string {
 }
 
 func (s Storage) Validate(c vpath.ContextPath) (r report.Report) {
+	s.validateDirectories(c, &r)
+	s.validateFiles(c, &r)
+	s.validateLinks(c, &r)
+	s.validateFilesystems(c, &r)
+	return
+}
+
+func (s Storage) validateDirectories(c vpath.ContextPath, r *report.Report) {
 	for i, d := range s.Directories {
 		for _, l := range s.Links {
 			if strings.HasPrefix(d.Path, l.Path+"/") {
@@ -41,6 +49,9 @@ func (s Storage) Validate(c vpath.ContextPath) (r report.Report) {
 			}
 		}
 	}
+}
+
+func (s Storage) validateFiles(c vpath.ContextPath, r *report.Report) {
 	for i, f := range s.Files {
 		for _, l := range s.Links {
 			if strings.HasPrefix(f.Path, l.Path+"/") {
@@ -48,6 +59,9 @@ func (s Storage) Validate(c vpath.ContextPath) (r report.Report) {
 			}
 		}
 	}
+}
+
+func (s Storage) validateLinks(c vpath.ContextPath, r *report.Report) {
 	for i, l1 := range s.Links {
 		for _, l2 := range s.Links {
 			if strings.HasPrefix(l1.Path, l2.Path+"/") {
@@ -76,6 +90,9 @@ func (s Storage) Validate(c vpath.ContextPath) (r report.Report) {
 		ownerCheck(l1.Group.ID == nil, c.Append("links", i, "group", "id"))
 		ownerCheck(l1.Group.Name == nil, c.Append("links", i, "group", "name"))
 	}
+}
+
+func (s Storage) validateFilesystems(c vpath.ContextPath, r *report.Report) {
 	disks := make(map[string]Disk)
 	for _, d := range s.Disks {
 		disks[d.Device] = d
@@ -91,5 +108,4 @@ func (s Storage) Validate(c vpath.ContextPath) (r report.Report) {
 			}
 		}
 	}
-	return
 }
