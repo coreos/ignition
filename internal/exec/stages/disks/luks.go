coreos/ignition

commit 1df22385190ad55fbde8160998f8323a50d34a08

internal/exec/stages/disks/luks: Log previously ignored errors
Those errors are unlikely to happen and neither is fatal nor a bug as
the file is written in a temporary folder in the initramfs that is
removed before moving to the real root.

Thus log them and ignore them for as we don't want to start failing on
those if we did not before.

Fixes lint:
```
Error return value of `keyFile.Close` is not checked (errcheck)
Error return value of `os.Remove` is not checked (errcheck)
```

internal/exec/stages/disks/luks.go
@@ -128,8 +128,14 @@ func (s *stage) createLuks(config types.Config) error {
 			return fmt.Errorf("creating keyfile: %w", err)
 		}
 		keyFilePath := keyFile.Name()
-		keyFile.Close()
-		defer os.Remove(keyFilePath)
+		if err := keyFile.Close(); err != nil {
+			s.Warning("could not close file %s: %v", keyFilePath, err)
+		}
+		defer func() {
+			if err = os.Remove(keyFilePath); err != nil {
+				s.Warning("could not remove file %s: %v", keyFilePath, err)
+			}
+		}()
 
 		if luks.Cex.IsPresent() {
 			// each LUKS device has associated keyfiles

commit 008fe5a1601d540dba148e796f7ef81c22143027

luks: fix occasional cex.key file removal
https://github.com/coreos/ignition/commit/1df22385190ad55fbde8160998f8323a50d34a08 broke CEX:
```
[   30.496802] ignition-ostree-growfs[1118]: + cryptsetup resize root --key-file /etc/luks/cex.key
[   30.501834] ignition-ostree-growfs[1257]: Failed to open key file.
```

Issue: https://github.com/coreos/rhel-coreos-config/issues/76

internal/exec/stages/disks/luks.go
@@ -132,11 +132,13 @@ func (s *stage) createLuks(config types.Config) error {
 			if err := keyFile.Close(); err != nil {
 				s.Warning("could not close file %s: %v", keyFilePath, err)
 			}
-			defer func() {
-				if err = os.Remove(keyFilePath); err != nil {
-					s.Warning("could not remove file %s: %v", keyFilePath, err)
+			// We must pass 'keyFilePath' as a parameter here, since it may change below (cex).
+			// Otherwise, the deferred function will see the modified value instead of the original one.
+			defer func(name string) {
+				if removeErr := os.Remove(name); removeErr != nil {
+					s.Warning("could not remove file %s: %v", name, removeErr)
 				}
-			}()
+			}(keyFilePath)
 
 			if luks.Cex.IsPresent() {
 				// each LUKS device has associated keyfiles