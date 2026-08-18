affa66a3f768a2592d530401717a9293a9b5bb8e
MrCode-2005 <vishnusajeev2005@gmail.com>
2026-08-18 12:54:33 +0530

    Revert changes to frozen config/v3_6 spec

--- config/v3_6/types/partition.go
+++ config/v3_6/types/partition.go
@@ -46,7 +46,7 @@ func (p Partition) Key() string {
 
 func (p Partition) Validate(c path.ContextPath) (r report.Report) {
 	if util.IsFalse(p.ShouldExist) &&
-		(util.NotEmpty(p.TypeGUID) || util.NotEmpty(p.GUID) || p.StartMiB != nil || p.SizeMiB != nil) {
+		(p.Label != nil || util.NotEmpty(p.TypeGUID) || util.NotEmpty(p.GUID) || p.StartMiB != nil || p.SizeMiB != nil) {
 		r.AddOnError(c, errors.ErrShouldNotExistWithOthers)
 	}
 	if p.Number == 0 && p.Label == nil {
